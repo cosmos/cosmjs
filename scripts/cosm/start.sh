813d3a351e904e1878c68ae2e53a7a9037ab6d9b
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-01-22 15:46:04 +0100

    Fix start and stop scripts

--- scripts/cosm/start.sh
+++ scripts/cosm/start.sh
@@ -12,15 +12,14 @@ echo "Using temporary dir $TMP_DIR"
 WASMD_LOGFILE="$TMP_DIR/wasmd.log"
 REST_SERVER_LOGFILE="$TMP_DIR/rest-server.log"
 
-HOME_DIR="/root"
-
 # This starts up wasmd
 docker volume rm -f wasmd_data
 docker run --rm \
     --name "$CONTAINER_NAME" \
+    -p 1317:1317 \
     -p 26657:26657 \
     -p 26656:26656 \
-    --mount type=bind,source=$(pwd)/template,target=/template \
+    --mount type=bind,source="$(pwd)/template",target=/template \
     --mount type=volume,source=wasmd_data,target=/root \
     "$CONTAINER_NAME:$VERSION" \
     ./run_wasmd.sh /template \
@@ -32,7 +31,7 @@ sleep 10
 
 docker exec "$CONTAINER_NAME" \
   wasmcli rest-server \
-  --node tcp://localhost:46657 \
+  --node tcp://localhost:26657 \
   --trust-node \
   --laddr tcp://0.0.0.0:1317 \
   > "$REST_SERVER_LOGFILE" &
--- scripts/cosm/stop.sh
+++ scripts/cosm/stop.sh
@@ -2,7 +2,7 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
-GAIAD_CONTAINER_NAME="gaiad"
+CONTAINER_NAME="wasmd"
 
 echo "Killing Cosmos container..."
-docker container kill "$GAIAD_CONTAINER_NAME"
+docker container kill "$CONTAINER_NAME"
