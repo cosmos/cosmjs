6444be5aa4a8636201d692d427a574e1b8749957
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-02-03 18:42:36 +0100

    Remove obsolete comments

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,4 +1,2 @@
-// export { coinToDecimal } from "./decoding";
-// export { decimalToCoin } from "./encoding";
 export { RestClient, TxsResponse } from "./restclient";
 export { AminoTx, isAminoStdTx } from "./types";
