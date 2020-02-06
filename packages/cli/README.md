b5d608af10098cbb0f3276188b65553d5dbf9342
Simon Warta <simon@warta.it>
2020-02-06 15:06:43 +0100

    Add @cosmwasm/cli

---
+++ packages/cli/README.md
@@ -0,0 +1,50 @@
+# @cosmwasm/cli
+
+[![npm version](https://img.shields.io/npm/v/@cosmwasm/cli.svg)](https://www.npmjs.com/package/@cosmwasm/cli)
+
+## Installation and first run
+
+The `cosmwasm-cli` executable is available via npm. We recommend local
+installations to your demo project. If you don't have one yet, just
+`mkdir cosmwasm-cli-installation && cd cosmwasm-cli-installation && yarn init --yes`.
+
+### locally with yarn
+
+```
+$ yarn add @cosmwasm/cli --dev
+$ ./node_modules/.bin/cosmwasm-cli
+```
+
+### locally with npm
+
+```
+$ npm install @cosmwasm/cli --save-dev
+$ ./node_modules/.bin/cosmwasm-cli
+```
+
+### globally with yarn
+
+```
+$ yarn global add @cosmwasm/cli
+$ cosmwasm-cli
+```
+
+### globally with npm
+
+```
+$ npm install -g @cosmwasm/cli
+$ cosmwasm-cli
+```
+
+## Getting started
+
+1. Install `@cosmwasm/cli` and run `cosmwasm-cli` as shown above
+2. TODO: write README inspired by
+   https://github.com/iov-one/iov-core/blob/master/packages/iov-cli/README.md
+
+## License
+
+This package is part of the cosmwasm-js repository, licensed under the Apache
+License 2.0 (see
+[NOTICE](https://github.com/confio/cosmwasm-js/blob/master/NOTICE) and
+[LICENSE](https://github.com/confio/cosmwasm-js/blob/master/LICENSE)).
---
+++ packages/cli/src/async.ts
@@ -0,0 +1,47 @@
+import * as recast from "recast";
+
+import babylon = require("babylon");
+
+export function wrapInAsyncFunction(code: string): string {
+  const codeInAsyncFunction = `(async () => {
+    ${code}
+  })()`;
+
+  const ast = recast.parse(codeInAsyncFunction, { parser: babylon });
+  const body = ast.program.body[0].expression.callee.body.body;
+
+  if (body.length !== 0) {
+    const last = body.pop();
+    if (last.type === "ExpressionStatement") {
+      body.push({
+        type: "ReturnStatement",
+        argument: last,
+      });
+    } else {
+      body.push(last);
+    }
+  }
+
+  // Remove var, let, const from variable declarations to make them available in context
+  // tslint:disable-next-line:no-object-mutation
+  ast.program.body[0].expression.callee.body.body = body.map((node: any) => {
+    if (node.type === "VariableDeclaration") {
+      return {
+        type: "ExpressionStatement",
+        expression: {
+          type: "SequenceExpression",
+          expressions: node.declarations.map((declaration: any) => ({
+            type: "AssignmentExpression",
+            operator: "=",
+            left: declaration.id,
+            right: declaration.init,
+          })),
+        },
+      };
+    } else {
+      return node;
+    }
+  });
+
+  return recast.print(ast).code;
+}
