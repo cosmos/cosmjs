c354af68d91317c3bf617e6f24c84da64ca53c89
Simon Warta <simon@warta.it>
2020-02-07 16:30:09 +0100

    Export logs module from SDK

--- packages/sdk/types/index.d.ts
+++ packages/sdk/types/index.d.ts
@@ -1,7 +1,8 @@
+import * as logs from "./logs";
 import * as types from "./types";
+export { logs, types };
 export { CosmosBech32Prefix, decodeBech32Pubkey, encodeAddress, isValidAddress } from "./address";
 export { unmarshalTx } from "./decoding";
 export { encodeSecp256k1Signature, makeSignBytes, marshalTx } from "./encoding";
 export { RestClient, TxsResponse } from "./restclient";
 export { makeCosmoshubPath, Pen, PrehashType, Secp256k1Pen } from "./pen";
-export { types };
