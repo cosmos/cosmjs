736b27afaa126ef397a147793015de6de3367ae1
Simon Warta <simon@warta.it>
2020-02-08 20:16:43 +0100

    Add address generator example

---
+++ packages/cli/examples/generate_address.ts
@@ -0,0 +1,8 @@
+const mnemonic = Bip39.encode(Random.getBytes(16)).toString();
+const pen = await Secp256k1Pen.fromMnemonic(mnemonic);
+const pubkey = encodeSecp256k1Pubkey(pen.pubkey);
+const address = encodeAddress(pubkey, "cosmos");
+
+console.info("mnemonic:", mnemonic);
+console.info("pubkey:", pubkey);
+console.info("address:", address);
