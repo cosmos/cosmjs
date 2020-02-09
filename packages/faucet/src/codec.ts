615892e37fdd50cbdd5ba20751916d4fff639f8a
Simon Warta <simon@warta.it>
2020-02-09 19:32:37 +0100

    Adapt faucet code to change in TokenConfiguration

--- packages/faucet/src/codec.ts
+++ packages/faucet/src/codec.ts
@@ -2,27 +2,29 @@ import { CosmWasmCodec, CosmWasmConnection, TokenConfiguration } from "@cosmwasm
 import { TxCodec } from "@iov/bcp";
 
 const prefix = "cosmos";
-const tokens: TokenConfiguration = [
-  {
-    fractionalDigits: 6,
-    name: "Fee Token",
-    ticker: "COSM",
-    denom: "cosm",
-  },
-  {
-    fractionalDigits: 6,
-    name: "Staking Token",
-    ticker: "STAKE",
-    denom: "stake",
-  },
-];
+const config: TokenConfiguration = {
+  bankTokens: [
+    {
+      fractionalDigits: 6,
+      name: "Fee Token",
+      ticker: "COSM",
+      denom: "cosm",
+    },
+    {
+      fractionalDigits: 6,
+      name: "Staking Token",
+      ticker: "STAKE",
+      denom: "stake",
+    },
+  ],
+};
 
 export async function establishConnection(url: string): Promise<CosmWasmConnection> {
-  return CosmWasmConnection.establish(url, prefix, tokens);
+  return CosmWasmConnection.establish(url, prefix, config);
 }
 
 export function codecImplementation(): TxCodec {
-  return new CosmWasmCodec(prefix, tokens);
+  return new CosmWasmCodec(prefix, config.bankTokens);
 }
 
 export function codecDefaultFractionalDigits(): number {
