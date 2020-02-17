f8051b93dc3c5ac9bfd480dd4eae4c7e4c6cc2a1
Simon Warta <simon@warta.it>
2020-02-17 12:36:55 +0100

    Use BCP type in decodeCosmosPubkey

--- packages/bcp/types/address.d.ts
+++ packages/bcp/types/address.d.ts
@@ -1,9 +1,4 @@
 import { CosmosAddressBech32Prefix } from "@cosmwasm/sdk";
-import { Address, Algorithm, PubkeyBundle, PubkeyBytes } from "@iov/bcp";
-export declare function decodeCosmosPubkey(
-  encodedPubkey: string,
-): {
-  readonly algo: Algorithm;
-  readonly data: PubkeyBytes;
-};
+import { Address, PubkeyBundle } from "@iov/bcp";
+export declare function decodeCosmosPubkey(encodedPubkey: string): PubkeyBundle;
 export declare function pubkeyToAddress(pubkey: PubkeyBundle, prefix: CosmosAddressBech32Prefix): Address;
