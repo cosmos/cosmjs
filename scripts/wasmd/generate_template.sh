e64becb7cf229c2bd0a5cec04d046b2046a6411f
Simon Warta <simon@warta.it>
2020-05-07 10:39:19 +0200

    Add more test addresses

--- scripts/wasmd/generate_template.sh
+++ scripts/wasmd/generate_template.sh
@@ -16,7 +16,9 @@ docker run --rm \
   --mount type=bind,source="$SCRIPT_DIR/template",target=/root \
   "$REPOSITORY:$VERSION" \
   ./setup.sh \
-  cosmos1pkptre7fdkl6gfrzlesjjvhxhlc3r4gmmk8rs6 cosmos1cjsxept9rkggzxztslae9ndgpdyt2408lk850u cosmos17d0jcz59jf68g52vq38tuuncmwwjk42u6mcxej
+  cosmos1pkptre7fdkl6gfrzlesjjvhxhlc3r4gmmk8rs6 cosmos10dyr9899g6t0pelew4nvf4j5c3jcgv0r73qga5 cosmos1xy4yqngt0nlkdcenxymg8tenrghmek4nmqm28k cosmos142u9fgcjdlycfcez3lw8x6x5h7rfjlnfhpw2lx cosmos1hsm76p4ahyhl5yh3ve9ur49r5kemhp2r0dcjvx \
+  cosmos1cjsxept9rkggzxztslae9ndgpdyt2408lk850u \
+  cosmos17d0jcz59jf68g52vq38tuuncmwwjk42u6mcxej
 
 # The ./template folder is created by the docker daemon's user (root on Linux, current user
 # when using Docker Desktop on macOS), let's make it ours if needed
