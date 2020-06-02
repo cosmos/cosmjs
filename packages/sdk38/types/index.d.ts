a0cb8011e15f926f162016c3ce0cb8869a9aeb8e
Simon Warta <simon@warta.it>
2020-06-02 13:18:31 +0200

    Export rawSecp256k1PubkeyToAddress, Pen, Secp256k1Pen, makeCosmoshubPath

--- packages/sdk38/types/index.d.ts
+++ packages/sdk38/types/index.d.ts
@@ -1,11 +1,12 @@
 import * as logs from "./logs";
 import * as types from "./types";
 export { logs, types };
-export { pubkeyToAddress } from "./address";
+export { pubkeyToAddress, rawSecp256k1PubkeyToAddress } from "./address";
 export { Coin, coin, coins } from "./coins";
 export { unmarshalTx } from "./decoding";
 export { makeSignBytes, marshalTx } from "./encoding";
 export { BroadcastMode, RestClient, TxsResponse } from "./restclient";
+export { Pen, Secp256k1Pen, makeCosmoshubPath } from "./pen";
 export { decodeBech32Pubkey, encodeBech32Pubkey, encodeSecp256k1Pubkey } from "./pubkey";
 export { findSequenceForSignedTx } from "./sequence";
 export { encodeSecp256k1Signature, decodeSignature } from "./signature";
