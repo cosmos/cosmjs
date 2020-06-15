90d16c61d83112e5993150d96896f34920c63f6b
willclarktech <willclarktech@users.noreply.github.com>
2020-06-15 13:38:02 +0100

    demo-protobuf: Add alternatives to generated static code

---
+++ packages/demo-protobuf/src/demo.ts
@@ -0,0 +1,7 @@
+import { Field, Root, Type } from "protobufjs";
+
+const MsgDemo = new Type("MsgDemo").add(new Field("example", 1, "string"));
+
+const root = new Root().define("demo").add(MsgDemo);
+
+export default root;
