ef413762011a41c8740342d65a68086eb94b389b
willclarktech <willclarktech@users.noreply.github.com>
2020-06-15 18:45:15 +0100

    demo-protobuf: Add basic registry example

--- packages/demo-protobuf/src/demo.ts
+++ packages/demo-protobuf/src/demo.ts
@@ -1,6 +1,6 @@
 import { Field, Root, Type } from "protobufjs";
 
-const MsgDemo = new Type("MsgDemo").add(new Field("example", 1, "string"));
+export const MsgDemo = new Type("MsgDemo").add(new Field("example", 1, "string"));
 
 const root = new Root().define("demo").add(MsgDemo);
 
