21e29d1f25379af2f902e98e0b6e94ee96eb71fb
willclarktech <willclarktech@users.noreply.github.com>
2020-06-16 10:19:19 +0100

    demo-protobuf: Add GeneratedType interface

--- packages/demo-protobuf/src/registry.ts
+++ packages/demo-protobuf/src/registry.ts
@@ -2,24 +2,21 @@ import protobuf from "protobufjs";
 
 import { cosmos_sdk as cosmosSdk, google } from "./generated/codecimpl";
 
-export type GeneratedType = {
-  readonly create: (properties?: { [k: string]: any }) => protobuf.Message<{}>;
-  readonly encode: (
-    message: protobuf.Message<{}> | { [k: string]: any },
-    writer?: protobuf.Writer,
-  ) => protobuf.Writer;
-  readonly decode: (reader: protobuf.Reader | Uint8Array, length?: number) => protobuf.Message<{}>;
-};
+interface GeneratedType {
+  readonly create: (properties?: { [k: string]: any }) => any;
+  readonly encode: (message: any | { [k: string]: any }, writer?: protobuf.Writer) => protobuf.Writer;
+  readonly decode: (reader: protobuf.Reader | Uint8Array, length?: number) => any;
+}
 
 export class Registry {
   private readonly types: Map<string, GeneratedType>;
 
   constructor(customTypes: Iterable<[string, GeneratedType]> = []) {
     this.types = new Map<string, GeneratedType>([
-      ["/cosmos.Coin", (cosmosSdk.v1.Coin as unknown) as GeneratedType],
-      ["/cosmos.bank.MsgSend", (cosmosSdk.x.bank.v1.MsgSend as unknown) as GeneratedType],
-      ["/cosmos.tx.TxBody", (cosmosSdk.tx.v1.TxBody as unknown) as GeneratedType],
-      ["/google.protobuf.Any", (google.protobuf.Any as unknown) as GeneratedType],
+      ["/cosmos.Coin", cosmosSdk.v1.Coin],
+      ["/cosmos.bank.MsgSend", cosmosSdk.x.bank.v1.MsgSend],
+      ["/cosmos.tx.TxBody", cosmosSdk.tx.v1.TxBody],
+      ["/google.protobuf.Any", google.protobuf.Any],
       ...customTypes,
     ]);
   }
