44bc903d4607fb64766495b093d0946b5c6c509c
willclarktech <willclarktech@users.noreply.github.com>
2020-06-16 09:47:05 +0100

    demo-protobuf: Fix type of demo.proto

--- packages/demo-protobuf/src/demo.proto.ts
+++ packages/demo-protobuf/src/demo.proto.ts
@@ -1,4 +1,6 @@
-export default `
+// Without this TS infers this is a string literal.
+// eslint-disable-next-line @typescript-eslint/no-inferrable-types
+const proto: string = `
 syntax = "proto3";
 package demo;
 
@@ -6,3 +8,5 @@ message MsgDemo {
   string example = 1;
 }
 `;
+
+export default proto;
--- packages/demo-protobuf/types/demo.proto.d.ts
+++ packages/demo-protobuf/types/demo.proto.d.ts
@@ -1,2 +1,2 @@
-declare const _default: '\nsyntax = "proto3";\npackage demo;\n\nmessage MsgDemo {\n  string example = 1;\n}\n';
-export default _default;
+declare const proto: string;
+export default proto;
