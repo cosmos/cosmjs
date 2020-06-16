19a9dfc40a1986fa1478b81ed4d846d888a308ad
willclarktech <willclarktech@users.noreply.github.com>
2020-06-16 10:19:19 +0100

    demo-protobuf: Add GeneratedType interface

--- packages/demo-protobuf/types/registry.d.ts
+++ packages/demo-protobuf/types/registry.d.ts
@@ -1,18 +1,19 @@
 import protobuf from "protobufjs";
-export declare type GeneratedType = {
-  readonly create: (properties?: { [k: string]: any }) => protobuf.Message<{}>;
+interface GeneratedType {
+  readonly create: (properties?: { [k: string]: any }) => any;
   readonly encode: (
     message:
-      | protobuf.Message<{}>
+      | any
       | {
           [k: string]: any;
         },
     writer?: protobuf.Writer,
   ) => protobuf.Writer;
-  readonly decode: (reader: protobuf.Reader | Uint8Array, length?: number) => protobuf.Message<{}>;
-};
+  readonly decode: (reader: protobuf.Reader | Uint8Array, length?: number) => any;
+}
 export declare class Registry {
   private readonly types;
   constructor(customTypes?: Iterable<[string, GeneratedType]>);
   lookupType(name: string): GeneratedType | undefined;
 }
+export {};
