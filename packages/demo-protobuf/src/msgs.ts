d1a069e94e8e6268c8ae8bcc28a78183e8558434
Simon Warta <simon@warta.it>
2020-06-23 15:15:33 +0200

    Implement MsgSend using decorators

---
+++ packages/demo-protobuf/src/msgs.ts
@@ -0,0 +1,27 @@
+import { Message } from "protobufjs";
+
+import { cosmosField, cosmosMessage } from "./decorator";
+import { Registry } from "./registry";
+
+export const defaultRegistry = new Registry();
+
+@cosmosMessage(defaultRegistry, "/cosmos.Coin")
+export class Coin extends Message<{}> {
+  @cosmosField.string(1)
+  public readonly denom?: string;
+
+  @cosmosField.string(2)
+  public readonly amount?: string;
+}
+
+@cosmosMessage(defaultRegistry, "/cosmos.bank.MsgSend")
+export class MsgSend extends Message<{}> {
+  @cosmosField.bytes(1)
+  public readonly from_address?: Uint8Array;
+
+  @cosmosField.bytes(2)
+  public readonly to_address?: Uint8Array;
+
+  @cosmosField.repeatedNested(3, Coin)
+  public readonly amount?: readonly Coin[];
+}
