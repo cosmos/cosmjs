9f0372889221c926a4bd08f0ae56bf9c82fe16e8
willclarktech <willclarktech@users.noreply.github.com>
2020-06-24 17:18:51 +0200

    socket: Fork @iov/socket

---
+++ packages/socket/README.md
@@ -0,0 +1,10 @@
+# @iov/socket
+
+[![npm version](https://img.shields.io/npm/v/@iov/socket.svg)](https://www.npmjs.com/package/@iov/socket)
+
+@iov/socket are some helper methods and classes for working with WebSockets.
+
+## License
+
+This package is part of the IOV-Core repository, licensed under the Apache License 2.0
+(see [NOTICE](https://github.com/iov-one/iov-core/blob/master/NOTICE) and [LICENSE](https://github.com/iov-one/iov-core/blob/master/LICENSE)).
---
+++ packages/socket/src/streamingsocket.spec.ts
@@ -0,0 +1,96 @@
+import { toListPromise } from "@iov/stream";
+
+import { StreamingSocket } from "./streamingsocket";
+
+function skipTests(): boolean {
+  return !process.env.SOCKETSERVER_ENABLED;
+}
+
+function pendingWithoutSocketServer(): void {
+  if (skipTests()) {
+    pending("Set SOCKETSERVER_ENABLED to enable socket tests");
+  }
+}
+
+describe("StreamingSocket", () => {
+  const socketServerUrl = "ws://localhost:4444/websocket";
+  const socketServerUrlSlow = "ws://localhost:4445/websocket";
+
+  it("can be constructed", () => {
+    const socket = new StreamingSocket(socketServerUrl);
+    expect(socket).toBeTruthy();
+  });
+
+  it("can connect", async () => {
+    pendingWithoutSocketServer();
+
+    const socket = new StreamingSocket(socketServerUrl);
+    expect(socket).toBeTruthy();
+    socket.connect();
+    await socket.connected;
+    socket.disconnect();
+  });
+
+  it("can connect to slow server", async () => {
+    pendingWithoutSocketServer();
+
+    const socket = new StreamingSocket(socketServerUrlSlow);
+    expect(socket).toBeTruthy();
+    socket.connect();
+    await socket.connected;
+    socket.disconnect();
+  });
+
+  it("times out when establishing connection takes too long", async () => {
+    pendingWithoutSocketServer();
+
+    const socket = new StreamingSocket(socketServerUrlSlow, 2_000);
+    socket.connect();
+
+    await socket.connected
+      .then(() => fail("must not resolve"))
+      .catch((error) => expect(error).toMatch(/connection attempt timed out/i));
+  });
+
+  it("can send events when connected", async () => {
+    pendingWithoutSocketServer();
+
+    const socket = new StreamingSocket(socketServerUrl);
+
+    const responsePromise = toListPromise(socket.events, 3);
+
+    socket.connect();
+    await socket.connected;
+
+    await socket.send("aabbccdd");
+    await socket.send("whatever");
+    await socket.send("lalala");
+
+    const response = await responsePromise;
+    expect(response.length).toEqual(3);
+
+    socket.disconnect();
+  });
+
+  it("completes stream when disconnected", (done) => {
+    pendingWithoutSocketServer();
+
+    const socket = new StreamingSocket(socketServerUrl);
+    expect(socket).toBeTruthy();
+    const subscription = socket.events.subscribe({
+      complete: () => {
+        subscription.unsubscribe();
+        done();
+      },
+    });
+
+    (async () => {
+      socket.connect();
+      await socket.connected;
+      await socket.send("aabbccdd");
+      await socket.send("whatever");
+      await socket.send("lalala");
+      socket.disconnect();
+    })().catch(done.fail);
+  });
+});
