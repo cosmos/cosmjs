9cd8371f7ddf263d0a0d62bbbd6310c87e1cbf26
Simon Warta <simon@warta.it>
2020-06-24 10:24:36 +0200

    Remove duplicate parts of "postTx" tests

--- packages/cosmwasm/src/restclient.spec.ts
+++ packages/cosmwasm/src/restclient.spec.ts
@@ -3,24 +3,17 @@ import { Sha256 } from "@cosmjs/crypto";
 import { Bech32, fromAscii, fromBase64, fromHex, toAscii, toBase64, toHex } from "@cosmjs/encoding";
 import {
   Coin,
-  encodeBech32Pubkey,
-  makeCosmoshubPath,
   makeSignBytes,
   Msg,
-  MsgSend,
   Pen,
   PostTxsResponse,
-  rawSecp256k1PubkeyToAddress,
   Secp256k1Pen,
   StdFee,
   StdSignature,
   StdTx,
-  TxsResponse,
 } from "@cosmjs/sdk38";
-import { assert, sleep } from "@cosmjs/utils";
-import { ReadonlyDate } from "readonly-date";
+import { assert } from "@cosmjs/utils";
 
-import { isPostTxFailure } from "./cosmwasmclient";
 import { findAttribute, parseLogs } from "./logs";
 import {
   isMsgInstantiateContract,
@@ -30,7 +23,6 @@ import {
   MsgStoreCode,
 } from "./msgs";
 import { RestClient } from "./restclient";
-import { SigningCosmWasmClient } from "./signingcosmwasmclient";
 import {
   alice,
   bech32AddressMatcher,
@@ -39,19 +31,10 @@ import {
   fromOneElementArray,
   getHackatom,
   makeRandomAddress,
-  nonNegativeIntegerMatcher,
   pendingWithoutWasmd,
-  semverMatcher,
-  tendermintAddressMatcher,
-  tendermintIdMatcher,
-  tendermintOptionalIdMatcher,
-  tendermintShortHashMatcher,
-  unused,
   wasmd,
   wasmdEnabled,
 } from "./testutils.spec";
-
-const emptyAddress = "cosmos1ltkhnmdcqemmd2tkhnx7qx66tq7e0wykw2j85k";
 
 function makeSignedTx(firstMsg: Msg, fee: StdFee, memo: string, firstSignature: StdSignature): StdTx {
   return {
@@ -273,362 +256,6 @@ describe("RestClient", () => {
   });
 
   describe("postTx", () => {
-    it("can send tokens", async () => {
-      pendingWithoutWasmd();
-      const pen = await Secp256k1Pen.fromMnemonic(alice.mnemonic);
-
-      const memo = "My first contract on chain";
-      const theMsg: MsgSend = {
-        type: "cosmos-sdk/MsgSend",
-        value: {
-          from_address: alice.address0,
-          to_address: emptyAddress,
-          amount: [
-            {
-              denom: "ucosm",
-              amount: "1234567",
-            },
-          ],
-        },
-      };
-
-      const fee: StdFee = {
-        amount: [
-          {
-            amount: "5000",
-            denom: "ucosm",
-          },
-        ],
-        gas: "890000",
-      };
-
-      const client = new RestClient(wasmd.endpoint);
-      const { account_number, sequence } = (await client.authAccounts(alice.address0)).result.value;
-
-      const signBytes = makeSignBytes([theMsg], fee, wasmd.chainId, memo, account_number, sequence);
-      const signature = await pen.sign(signBytes);
-      const signedTx = makeSignedTx(theMsg, fee, memo, signature);
-      const result = await client.postTx(signedTx);
-      expect(result.code).toBeUndefined();
-      expect(result).toEqual({
-        height: jasmine.stringMatching(nonNegativeIntegerMatcher),
-        txhash: jasmine.stringMatching(tendermintIdMatcher),
-        // code is not set
-        raw_log: jasmine.stringMatching(/^\[.+\]$/i),
-        logs: jasmine.any(Array),
-        gas_wanted: jasmine.stringMatching(nonNegativeIntegerMatcher),
-        gas_used: jasmine.stringMatching(nonNegativeIntegerMatcher),
-      });
-    });
-
-    it("can't send transaction with additional signatures", async () => {
-      pendingWithoutWasmd();
-      const account1 = await Secp256k1Pen.fromMnemonic(alice.mnemonic, makeCosmoshubPath(0));
-      const account2 = await Secp256k1Pen.fromMnemonic(alice.mnemonic, makeCosmoshubPath(1));
-      const account3 = await Secp256k1Pen.fromMnemonic(alice.mnemonic, makeCosmoshubPath(2));
-      const address1 = rawSecp256k1PubkeyToAddress(account1.pubkey, "cosmos");
-      const address2 = rawSecp256k1PubkeyToAddress(account2.pubkey, "cosmos");
-      const address3 = rawSecp256k1PubkeyToAddress(account3.pubkey, "cosmos");
-
-      const memo = "My first contract on chain";
-      const theMsg: MsgSend = {
-        type: "cosmos-sdk/MsgSend",
-        value: {
-          from_address: address1,
-          to_address: emptyAddress,
-          amount: [
-            {
-              denom: "ucosm",
-              amount: "1234567",
-            },
-          ],
-        },
-      };
-
-      const fee: StdFee = {
-        amount: [
-          {
-            amount: "5000",
-            denom: "ucosm",
-          },
-        ],
-        gas: "890000",
-      };
-
-      const client = new RestClient(wasmd.endpoint);
-      const { account_number: an1, sequence: sequence1 } = (await client.authAccounts(address1)).result.value;
-      const { account_number: an2, sequence: sequence2 } = (await client.authAccounts(address2)).result.value;
-      const { account_number: an3, sequence: sequence3 } = (await client.authAccounts(address3)).result.value;
-
-      const signBytes1 = makeSignBytes([theMsg], fee, wasmd.chainId, memo, an1, sequence1);
-      const signBytes2 = makeSignBytes([theMsg], fee, wasmd.chainId, memo, an2, sequence2);
-      const signBytes3 = makeSignBytes([theMsg], fee, wasmd.chainId, memo, an3, sequence3);
-      const signature1 = await account1.sign(signBytes1);
-      const signature2 = await account2.sign(signBytes2);
-      const signature3 = await account3.sign(signBytes3);
-      const signedTx = {
-        msg: [theMsg],
-        fee: fee,
-        memo: memo,
-        signatures: [signature1, signature2, signature3],
-      };
-      const postResult = await client.postTx(signedTx);
-      expect(postResult.code).toEqual(4);
-      expect(postResult.raw_log).toContain("wrong number of signers");
-    });
-
-    it("can send multiple messages with one signature", async () => {
-      pendingWithoutWasmd();
-      const account1 = await Secp256k1Pen.fromMnemonic(alice.mnemonic, makeCosmoshubPath(0));
-      const address1 = rawSecp256k1PubkeyToAddress(account1.pubkey, "cosmos");
-
-      const memo = "My first contract on chain";
-      const msg1: MsgSend = {
-        type: "cosmos-sdk/MsgSend",
-        value: {
-          from_address: address1,
-          to_address: emptyAddress,
-          amount: [
-            {
-              denom: "ucosm",
-              amount: "1234567",
-            },
-          ],
-        },
-      };
-      const msg2: MsgSend = {
-        type: "cosmos-sdk/MsgSend",
-        value: {
-          from_address: address1,
-          to_address: emptyAddress,
-          amount: [
-            {
-              denom: "ucosm",
-              amount: "7654321",
-            },
-          ],
-        },
-      };
-
-      const fee: StdFee = {
-        amount: [
-          {
-            amount: "5000",
-            denom: "ucosm",
-          },
-        ],
-        gas: "890000",
-      };
-
-      const client = new RestClient(wasmd.endpoint);
-      const { account_number, sequence } = (await client.authAccounts(address1)).result.value;
-
-      const signBytes = makeSignBytes([msg1, msg2], fee, wasmd.chainId, memo, account_number, sequence);
-      const signature1 = await account1.sign(signBytes);
-      const signedTx = {
-        msg: [msg1, msg2],
-        fee: fee,
-        memo: memo,
-        signatures: [signature1],
-      };
-      const postResult = await client.postTx(signedTx);
-      expect(postResult.code).toBeUndefined();
-    });
-
-    it("can send multiple messages with multiple signatures", async () => {
-      pendingWithoutWasmd();
-      const account1 = await Secp256k1Pen.fromMnemonic(alice.mnemonic, makeCosmoshubPath(0));
-      const account2 = await Secp256k1Pen.fromMnemonic(alice.mnemonic, makeCosmoshubPath(1));
-      const address1 = rawSecp256k1PubkeyToAddress(account1.pubkey, "cosmos");
-      const address2 = rawSecp256k1PubkeyToAddress(account2.pubkey, "cosmos");
-
-      const memo = "My first contract on chain";
-      const msg1: MsgSend = {
-        type: "cosmos-sdk/MsgSend",
-        value: {
-          from_address: address1,
-          to_address: emptyAddress,
-          amount: [
-            {
-              denom: "ucosm",
-              amount: "1234567",
-            },
-          ],
-        },
-      };
-      const msg2: MsgSend = {
-        type: "cosmos-sdk/MsgSend",
-        value: {
-          from_address: address2,
-          to_address: emptyAddress,
-          amount: [
-            {
-              denom: "ucosm",
-              amount: "7654321",
-            },
-          ],
-        },
-      };
-
-      const fee: StdFee = {
-        amount: [
-          {
-            amount: "5000",
-            denom: "ucosm",
-          },
-        ],
-        gas: "890000",
-      };
-
-      const client = new RestClient(wasmd.endpoint);
-      const { account_number: an1, sequence: sequence1 } = (await client.authAccounts(address1)).result.value;
-      const { account_number: an2, sequence: sequence2 } = (await client.authAccounts(address2)).result.value;
-
-      const signBytes1 = makeSignBytes([msg2, msg1], fee, wasmd.chainId, memo, an1, sequence1);
-      const signBytes2 = makeSignBytes([msg2, msg1], fee, wasmd.chainId, memo, an2, sequence2);
-      const signature1 = await account1.sign(signBytes1);
-      const signature2 = await account2.sign(signBytes2);
-      const signedTx = {
-        msg: [msg2, msg1],
-        fee: fee,
-        memo: memo,
-        signatures: [signature2, signature1],
-      };
-      const postResult = await client.postTx(signedTx);
-      expect(postResult.code).toBeUndefined();
-
-      await sleep(500);
-      const searched = await client.txsQuery(`tx.hash=${postResult.txhash}`);
-      expect(searched.txs.length).toEqual(1);
-      expect(searched.txs[0].tx.value.signatures).toEqual([signature2, signature1]);
-    });
-
-    it("can't send transaction with wrong signature order (1)", async () => {
-      pendingWithoutWasmd();
-      const account1 = await Secp256k1Pen.fromMnemonic(alice.mnemonic, makeCosmoshubPath(0));
-      const account2 = await Secp256k1Pen.fromMnemonic(alice.mnemonic, makeCosmoshubPath(1));
-      const address1 = rawSecp256k1PubkeyToAddress(account1.pubkey, "cosmos");
-      const address2 = rawSecp256k1PubkeyToAddress(account2.pubkey, "cosmos");
-
-      const memo = "My first contract on chain";
-      const msg1: MsgSend = {
-        type: "cosmos-sdk/MsgSend",
-        value: {
-          from_address: address1,
-          to_address: emptyAddress,
-          amount: [
-            {
-              denom: "ucosm",
-              amount: "1234567",
-            },
-          ],
-        },
-      };
-      const msg2: MsgSend = {
-        type: "cosmos-sdk/MsgSend",
-        value: {
-          from_address: address2,
-          to_address: emptyAddress,
-          amount: [
-            {
-              denom: "ucosm",
-              amount: "7654321",
-            },
-          ],
-        },
-      };
-
-      const fee: StdFee = {
-        amount: [
-          {
-            amount: "5000",
-            denom: "ucosm",
-          },
-        ],
-        gas: "890000",
-      };
-
-      const client = new RestClient(wasmd.endpoint);
-      const { account_number: an1, sequence: sequence1 } = (await client.authAccounts(address1)).result.value;
-      const { account_number: an2, sequence: sequence2 } = (await client.authAccounts(address2)).result.value;
-
-      const signBytes1 = makeSignBytes([msg1, msg2], fee, wasmd.chainId, memo, an1, sequence1);
-      const signBytes2 = makeSignBytes([msg1, msg2], fee, wasmd.chainId, memo, an2, sequence2);
-      const signature1 = await account1.sign(signBytes1);
-      const signature2 = await account2.sign(signBytes2);
-      const signedTx = {
-        msg: [msg1, msg2],
-        fee: fee,
-        memo: memo,
-        signatures: [signature2, signature1],
-      };
-      const postResult = await client.postTx(signedTx);
-      expect(postResult.code).toEqual(8);
-    });
-
-    it("can't send transaction with wrong signature order (2)", async () => {
-      pendingWithoutWasmd();
-      const account1 = await Secp256k1Pen.fromMnemonic(alice.mnemonic, makeCosmoshubPath(0));
-      const account2 = await Secp256k1Pen.fromMnemonic(alice.mnemonic, makeCosmoshubPath(1));
-      const address1 = rawSecp256k1PubkeyToAddress(account1.pubkey, "cosmos");
-      const address2 = rawSecp256k1PubkeyToAddress(account2.pubkey, "cosmos");
-
-      const memo = "My first contract on chain";
-      const msg1: MsgSend = {
-        type: "cosmos-sdk/MsgSend",
-        value: {
-          from_address: address1,
-          to_address: emptyAddress,
-          amount: [
-            {
-              denom: "ucosm",
-              amount: "1234567",
-            },
-          ],
-        },
-      };
-      const msg2: MsgSend = {
-        type: "cosmos-sdk/MsgSend",
-        value: {
-          from_address: address2,
-          to_address: emptyAddress,
-          amount: [
-            {
-              denom: "ucosm",
-              amount: "7654321",
-            },
-          ],
-        },
-      };
-
-      const fee: StdFee = {
-        amount: [
-          {
-            amount: "5000",
-            denom: "ucosm",
-          },
-        ],
-        gas: "890000",
-      };
-
-      const client = new RestClient(wasmd.endpoint);
-      const { account_number: an1, sequence: sequence1 } = (await client.authAccounts(address1)).result.value;
-      const { account_number: an2, sequence: sequence2 } = (await client.authAccounts(address2)).result.value;
-
-      const signBytes1 = makeSignBytes([msg2, msg1], fee, wasmd.chainId, memo, an1, sequence1);
-      const signBytes2 = makeSignBytes([msg2, msg1], fee, wasmd.chainId, memo, an2, sequence2);
-      const signature1 = await account1.sign(signBytes1);
-      const signature2 = await account2.sign(signBytes2);
-      const signedTx = {
-        msg: [msg2, msg1],
-        fee: fee,
-        memo: memo,
-        signatures: [signature1, signature2],
-      };
-      const postResult = await client.postTx(signedTx);
-      expect(postResult.code).toEqual(8);
-    });
-
     it("can upload, instantiate and execute wasm", async () => {
       pendingWithoutWasmd();
       const pen = await Secp256k1Pen.fromMnemonic(alice.mnemonic);
