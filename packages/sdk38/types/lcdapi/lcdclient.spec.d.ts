7ebb9cbca71a24c60d0376af7a84ed43e6962a80
Simon Warta <simon@warta.it>
2020-07-07 16:51:24 +0200

    Remove spec types in sub-folders

--- packages/sdk38/types/lcdapi/lcdclient.spec.d.ts
+++
@@ -1,8 +0,0 @@
-/** Deployed as part of scripts/wasmd/init.sh */
-export declare const deployedErc20: {
-  codeId: number;
-  source: string;
-  builder: string;
-  checksum: string;
-  instances: string[];
-};
