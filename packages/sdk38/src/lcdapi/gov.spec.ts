9f651cd305ea8ffd42f938e02c2f8573aa2637ce
willclarktech <willclarktech@users.noreply.github.com>
2020-07-15 17:01:26 +0200

    sdk38: Switch to eslint naming-convention rule in gov module

--- packages/sdk38/src/lcdapi/gov.spec.ts
+++ packages/sdk38/src/lcdapi/gov.spec.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { sleep } from "@cosmjs/utils";
 
 import { coins } from "../coins";
