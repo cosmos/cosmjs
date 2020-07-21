04c753590401f2587caf4874472a0d74ca14a868
willclarktech <willclarktech@users.noreply.github.com>
2020-07-21 13:48:19 +0200

    sdk38: Update eslint overrides in LCD staking module

--- packages/sdk38/src/lcdapi/staking.spec.ts
+++ packages/sdk38/src/lcdapi/staking.spec.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { nonNegativeIntegerMatcher, pendingWithoutWasmd, wasmd } from "../testutils.spec";
 import { LcdClient } from "./lcdclient";
 import { setupStakingExtension, StakingExtension } from "./staking";
