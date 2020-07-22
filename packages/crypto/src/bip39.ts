325a28d8ecc1c6d3d9999e1b811546e5a6fb08b5
Simon Warta <simon@warta.it>
2020-07-22 08:28:49 +0200

    Improve Bip39.encode docs

--- packages/crypto/src/bip39.ts
+++ packages/crypto/src/bip39.ts
@@ -6,6 +6,21 @@ import * as unorm from "unorm";
 import { EnglishMnemonic } from "./englishmnemonic";
 
 export class Bip39 {
+  /**
+   * Encodes raw entropy of length 16, 20, 24, 28 or 32 bytes as an English mnemonic between 12 and 24 words.
+   *
+   * | Entropy            | Words |
+   * |--------------------|-------|
+   * | 128 bit (16 bytes) |    12 |
+   * | 160 bit (20 bytes) |    15 |
+   * | 192 bit (24 bytes) |    18 |
+   * | 224 bit (28 bytes) |    21 |
+   * | 256 bit (32 bytes) |    24 |
+   *
+   *
+   * @see https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki#generating-the-mnemonic
+   * @param entropy The entropy to be encoded. This must be cryptographically secure.
+   */
   public static encode(entropy: Uint8Array): EnglishMnemonic {
     const allowedEntropyLengths: readonly number[] = [16, 20, 24, 28, 32];
 
