785ab9fc466aa5baaa76dcd26e54fdeb905c5d77
Simon Warta <simon@warta.it>
2020-07-29 11:51:13 +0200

    Update simapp to nightly-2020-07-29

--- scripts/simapp/setup.sh
+++ scripts/simapp/setup.sh
@@ -5,21 +5,26 @@ command -v shellcheck > /dev/null && shellcheck "$0"
 PASSWORD=${PASSWORD:-1234567890}
 STAKE=${STAKE_TOKEN:-ustake}
 FEE=${FEE_TOKEN:-ucosm}
-CHAIN_ID=${CHAIN_ID:-testing}
+CHAIN_ID=${CHAIN_ID:-simd-testing}
 
-simd init --chain-id="$CHAIN_ID" "$CHAIN_ID"
-# staking/governance token is hardcoded in config, change this
-sed -i "s/\"stake\"/\"$STAKE\"/" "$HOME"/.simapp/config/genesis.json
-if ! simcli keys show validator; then
-  (echo "$PASSWORD"; echo "$PASSWORD") | simcli keys add validator
+echo "Creating genesis ..."
+simd init --chain-id "$CHAIN_ID" "$CHAIN_ID"
+sed -i "s/\"stake\"/\"$STAKE\"/" "$HOME"/.simapp/config/genesis.json # staking/governance token is hardcoded in config, change this
+
+echo "Setting up validator ..."
+if ! simd keys show validator; then
+  (echo "$PASSWORD"; echo "$PASSWORD") | simd keys add validator
 fi
 # hardcode the validator account for this instance
 echo "$PASSWORD" | simd add-genesis-account validator "1000000000$STAKE,1000000000$FEE"
+
+echo "Setting up accounts ..."
 # (optionally) add a few more genesis accounts
 for addr in "$@"; do
   echo $addr
   simd add-genesis-account "$addr" "1000000000$STAKE,1000000000$FEE"
 done
-# submit a genesis validator tx
-(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | simd gentx --name validator --amount "250000000$STAKE"
+
+echo "Creating genesis tx ..."
+(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | simd gentx validator --amount "250000000$STAKE" --chain-id "$CHAIN_ID"
 simd collect-gentxs
