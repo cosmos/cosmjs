fcb213c39f326355f08620fa696d2c48fd204b52
Simon Warta <simon@warta.it>
2020-08-02 20:52:51 +0200

    Make simd ports more explicit

--- scripts/simapp/start.sh
+++ scripts/simapp/start.sh
@@ -2,6 +2,9 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
+TENDERMINT_PORT_GUEST="26657"
+TENDERMINT_PORT_HOST="26657"
+
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 # shellcheck source=./env
 # shellcheck disable=SC1091
@@ -13,21 +16,20 @@ TMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/simapp.XXXXXXXXX")
 chmod 777 "$TMP_DIR"
 echo "Using temporary dir $TMP_DIR"
 SIMD_LOGFILE="$TMP_DIR/simd.log"
-PORT=26657
 
 # Use a fresh volume for every start
 docker volume rm -f simapp_data
 
 docker run --rm \
   --name "$CONTAINER_NAME" \
-  -p "$PORT:$PORT" \
+  -p "$TENDERMINT_PORT_HOST":"$TENDERMINT_PORT_GUEST" \
   --mount type=bind,source="$SCRIPT_DIR/template",target=/template \
   --mount type=volume,source=simapp_data,target=/root \
   "$REPOSITORY:$VERSION" \
   /template/run_simd.sh \
   > "$SIMD_LOGFILE" &
 
-echo "simd running on http://localhost:$PORT and logging into $SIMD_LOGFILE"
+echo "simd running on http://localhost:$TENDERMINT_PORT_HOST and logging into $SIMD_LOGFILE"
 
 if [ -n "${CI:-}" ]; then
   # Follow the logs in CI's background job
