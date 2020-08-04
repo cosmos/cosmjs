8bb2b1bb766ac9dc7fcfa189d6391be05a714d36
Simon Warta <simon@warta.it>
2020-08-04 11:38:48 +0200

    Improve docs of isNonNullObject

--- packages/utils/types/typechecks.d.ts
+++ packages/utils/types/typechecks.d.ts
@@ -1,5 +1,10 @@
 /**
- * Checks if data is a non-null object (i.e. matches the TypeScript object type)
+ * Checks if data is a non-null object (i.e. matches the TypeScript object type).
+ *
+ * Note: this returns true for arrays, which are objects in JavaScript
+ * even though array and object are different types in JSON.
+ *
+ * @see https://www.typescriptlang.org/docs/handbook/basic-types.html#object
  */
 export declare function isNonNullObject(data: unknown): data is object;
 /**
