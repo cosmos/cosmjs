178214330ca754fcd5e6f18c552861ff20e6bf4c
willclarktech <willclarktech@users.noreply.github.com>
2020-08-05 12:21:33 +0200

    scripts: Update simapp template

--- scripts/simapp/template/setup.sh
+++ scripts/simapp/template/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -o errexit -o nounset -o pipefail
+set -o errexit -o nounset
 command -v shellcheck > /dev/null && shellcheck "$0"
 
 PASSWORD=${PASSWORD:-1234567890}
@@ -13,7 +13,7 @@ simd init --chain-id "$CHAIN_ID" "$MONIKER"
 sed -i "s/\"stake\"/\"$STAKE\"/" "$HOME"/.simapp/config/genesis.json # staking/governance token is hardcoded in config, change this
 
 echo "Setting up validator ..."
-if ! simd keys show validator; then
+if ! simd keys show validator 2> /dev/null; then
   echo "Validator does not yet exist. Creating it ..."
   (echo "$PASSWORD"; echo "$PASSWORD") | simd keys add validator
 fi
@@ -23,10 +23,10 @@ echo "$PASSWORD" | simd add-genesis-account validator "1000000000$STAKE,10000000
 echo "Setting up accounts ..."
 # (optionally) add a few more genesis accounts
 for addr in "$@"; do
-  echo $addr
+  echo "$addr"
   simd add-genesis-account "$addr" "1000000000$STAKE,1000000000$FEE"
 done
 
 echo "Creating genesis tx ..."
-(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | simd gentx validator --amount "250000000$STAKE" --chain-id "$CHAIN_ID"
+(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | simd gentx validator --offline --amount "250000000$STAKE" --chain-id "$CHAIN_ID" --moniker="$MONIKER"
 simd collect-gentxs
