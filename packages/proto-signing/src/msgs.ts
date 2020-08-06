2d7124de6ceb77321c56ec7cd8c0afcf062383cf
willclarktech <willclarktech@users.noreply.github.com>
2020-08-06 11:56:20 +0200

    Add signing demo tests

--- packages/proto-signing/src/msgs.ts
+++ packages/proto-signing/src/msgs.ts
@@ -26,3 +26,19 @@ export class MsgSend extends Message {
   @cosmosField.repeatedMessage(3, Coin)
   public readonly amount?: readonly Coin[];
 }
+
+// TODO: move somewhere else as needed
+@registered(defaultRegistry, "/cosmos.auth.BaseAccount")
+export class BaseAccount extends Message {
+  @cosmosField.bytes(1)
+  public readonly address?: Uint8Array;
+
+  @cosmosField.bytes(2)
+  public readonly pub_key?: Uint8Array;
+
+  @cosmosField.uint64(3)
+  public readonly account_number?: Long | number;
+
+  @cosmosField.uint64(4)
+  public readonly sequence?: Long | number;
+}
--- packages/proto-signing/types/msgs.d.ts
+++ packages/proto-signing/types/msgs.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="long" />
 import { Message } from "protobufjs";
 import { Registry } from "./registry";
 export declare const defaultRegistry: Registry;
@@ -9,4 +10,10 @@ export declare class MsgSend extends Message {
   readonly from_address?: Uint8Array;
   readonly to_address?: Uint8Array;
   readonly amount?: readonly Coin[];
+}
+export declare class BaseAccount extends Message {
+  readonly address?: Uint8Array;
+  readonly pub_key?: Uint8Array;
+  readonly account_number?: Long | number;
+  readonly sequence?: Long | number;
 }
