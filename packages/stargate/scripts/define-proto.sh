fb35426ae7462b65478d5a28533a25ebdf8d5a74
Simon Warta <simon@warta.it>
2020-08-10 07:00:20 +0200

    Add code generation to @cosmjs/stargate

---
+++ packages/stargate/scripts/define-proto.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
+TMP_DIR="./tmp"
+JS_SOURCE_FILE="$TMP_DIR/codecimpl.js"
+DEFINITIONS_FILE="$TMP_DIR/codecimpl.d.ts"
+OUTPUT_DIR="./src/generated/"
+
+
+yarn pbts "$JS_SOURCE_FILE" -o "$DEFINITIONS_FILE"
+yarn tsc --removeComments --outDir "$OUTPUT_DIR" --allowJs "$JS_SOURCE_FILE"
+cp "$DEFINITIONS_FILE" "$OUTPUT_DIR"
+rm "$DEFINITIONS_FILE" "$JS_SOURCE_FILE"
