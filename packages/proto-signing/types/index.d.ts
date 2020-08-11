f53425d7ff0b144f431bdf30308afd44655e5ea2
willclarktech <willclarktech@users.noreply.github.com>
2020-08-11 17:53:21 +0200

    proto-signing: Export omitDefaults and Registry from index

--- packages/proto-signing/types/index.d.ts
+++ packages/proto-signing/types/index.d.ts
@@ -1,3 +1,5 @@
+export { omitDefaults } from "./adr27";
 export { decodeAny } from "./any";
 export { Coin } from "./msgs";
 export { cosmosField } from "./decorator";
+export { Registry } from "./registry";
