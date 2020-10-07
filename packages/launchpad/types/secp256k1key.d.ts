53cc508910532041f9db231ec4ad54f2a0e4a38e
Shane Vitarana <shanev@gmail.com>
2020-10-07 17:59:59 -0400

    Revert "Renamed Secp256k1Key -> Secp256k1Wallet"
    This reverts commit 35a082701de9bd2c8cf89b25a168f12212d4ba8a.

--- packages/launchpad/types/secp256k1key.d.ts
+++ packages/launchpad/types/secp256k1key.d.ts
@@ -1,13 +1,13 @@
 import { StdSignDoc } from "./encoding";
 import { AccountData, OfflineSigner, SignResponse } from "./signer";
-export declare class Secp256k1Wallet implements OfflineSigner {
+export declare class Secp256k1Key implements OfflineSigner {
   /**
    * Creates a Secp256k1 key signer from the given private key
    *
    * @param privkey The private key.
    * @param prefix The bech32 address prefix (human readable part). Defaults to "cosmos".
    */
-  static fromPrivateKey(privkey: Uint8Array, prefix?: string): Promise<Secp256k1Wallet>;
+  static fromPrivateKey(privkey: Uint8Array, prefix?: string): Promise<Secp256k1Key>;
   private readonly pubkey;
   private readonly privkey;
   private readonly prefix;
