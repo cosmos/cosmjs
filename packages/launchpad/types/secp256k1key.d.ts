04a74aac839d2d748df416f244c8c861d68afb94
Shane Vitarana <shanev@gmail.com>
2020-10-07 12:03:07 -0400

    Add Secpk256k1 offline key signer
    Renamed Secp256k1Wallet -> Secp256k1HdWallet
    
    Renamed Secp256k1Key -> Secp256k1Wallet
    
    Renamed fromPrivateKey function
    
    Fixed linter issues
    
    Rename Secp256k1Wallet.fromMnemonic everywhere
    
    Revert "Rename Secp256k1Wallet.fromMnemonic everywhere"
    
    This reverts commit 18c52968bc1a234755c61d1d5c67850222239bd7.
    
    Revert "Fixed linter issues"
    
    This reverts commit 8e6fce93d809f160e8dafa9478f56f9effcf561a.
    
    Revert "Renamed fromPrivateKey function"
    
    This reverts commit d628f186bb126d4fc2b205dbc0b95b3198af02df.
    
    Revert "Renamed Secp256k1Key -> Secp256k1Wallet"
    
    This reverts commit 35a082701de9bd2c8cf89b25a168f12212d4ba8a.
    
    Revert "Renamed Secp256k1Wallet -> Secp256k1HdWallet"
    
    This reverts commit bd69f293e1ae7d96ad0afd295af360de70863e13.
    
    Ran linter
    
    Renamed fromPrivateKey
    
    Updated Typescript definitions

---
+++ packages/launchpad/types/secp256k1key.d.ts
@@ -0,0 +1,18 @@
+import { StdSignDoc } from "./encoding";
+import { AccountData, OfflineSigner, SignResponse } from "./signer";
+export declare class Secp256k1Key implements OfflineSigner {
+  /**
+   * Creates a Secp256k1 key signer from the given private key
+   *
+   * @param privkey The private key.
+   * @param prefix The bech32 address prefix (human readable part). Defaults to "cosmos".
+   */
+  static fromPrivkey(privkey: Uint8Array, prefix?: string): Promise<Secp256k1Key>;
+  private readonly pubkey;
+  private readonly privkey;
+  private readonly prefix;
+  private constructor();
+  private get address();
+  getAccounts(): Promise<readonly AccountData[]>;
+  sign(signerAddress: string, signDoc: StdSignDoc): Promise<SignResponse>;
+}
