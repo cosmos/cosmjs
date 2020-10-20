7d3aa4c9ea576c3522c5304c40114e4007d3437d
willclarktech <willclarktech@users.noreply.github.com>
2020-10-20 14:38:47 +0200

    proto-signing: Export registered decorator and EncodeObject type

--- packages/proto-signing/types/index.d.ts
+++ packages/proto-signing/types/index.d.ts
@@ -1,6 +1,6 @@
 export { Coin } from "./msgs";
-export { cosmosField } from "./decorator";
-export { Registry } from "./registry";
+export { cosmosField, registered } from "./decorator";
+export { EncodeObject, Registry } from "./registry";
 export { DirectSecp256k1Wallet } from "./directsecp256k1wallet";
 export { decodePubkey, encodePubkey } from "./pubkey";
 export { isOfflineDirectSigner, OfflineDirectSigner, OfflineSigner } from "./signer";
