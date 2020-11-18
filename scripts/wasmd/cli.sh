e97b36d4af7ff0d14522929b260b3e9468f25dc4
willclarktech <willclarktech@users.noreply.github.com>
2020-11-18 16:34:18 +0100

    scripts: Add basic Stargate-compatible wasmd files

---
+++ scripts/wasmd/cli.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+# shellcheck source=./env
+# shellcheck disable=SC1091
+source "$SCRIPT_DIR"/env
+
+# TODO: make this run as UID? Does this matter?
+HOME_DIR="/root"
+
+docker run \
+  --rm \
+  -it \
+  --mount type=volume,source=wasmd_data,target=/root/.wasmd \
+  -w "$HOME_DIR" \
+  --env "HOME=$HOME_DIR" \
+  --net "container:$CONTAINER_NAME" \
+  "$REPOSITORY:$VERSION" \
+  wasmd "$@"
---
+++ scripts/wasmd/stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+# shellcheck source=./env
+# shellcheck disable=SC1091
+source "$SCRIPT_DIR"/env
+
+echo "Killing Cosmos container..."
+docker container kill "$CONTAINER_NAME"
