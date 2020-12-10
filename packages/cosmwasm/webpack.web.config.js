65fe7fa4a2366ef6ed8d940c0524b3c92ede070f
willclarktech <willclarktech@users.noreply.github.com>
2020-12-10 17:51:25 +0000

    cosmwasm: Add CW1/CW3 env variables to webpack

--- packages/cosmwasm/webpack.web.config.js
+++ packages/cosmwasm/webpack.web.config.js
@@ -14,6 +14,8 @@ module.exports = [
       path: distdir,
       filename: "tests.js",
     },
-    plugins: [new webpack.EnvironmentPlugin(["LAUNCHPAD_ENABLED", "ERC20_ENABLED"])],
+    plugins: [
+      new webpack.EnvironmentPlugin(["LAUNCHPAD_ENABLED", "ERC20_ENABLED", "CW3_ENABLED", "CW1_ENABLED"]),
+    ],
   },
 ];
