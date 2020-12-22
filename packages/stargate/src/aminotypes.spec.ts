bf5fb7fca4b9255c42a514b6a1b48274b22d1204
willclarktech <willclarktech@users.noreply.github.com>
2020-12-22 16:59:29 +0000

    stargate: Actually use custom type urls in AminoTypes

--- packages/stargate/src/aminotypes.spec.ts
+++ packages/stargate/src/aminotypes.spec.ts
@@ -8,6 +8,18 @@ describe("AminoTypes", () => {
       expect(msgType).toEqual("cosmos-sdk/MsgDelegate");
     });
 
+    it("works with custom type url", () => {
+      const msgType = new AminoTypes({ "/my.CustomType": "my-sdk/CustomType" }).toAmino("/my.CustomType");
+      expect(msgType).toEqual("my-sdk/CustomType");
+    });
+
+    it("works with overridden type url", () => {
+      const msgType = new AminoTypes({
+        "/cosmos.staking.v1beta1.MsgDelegate": "my-override/MsgDelegate",
+      }).toAmino("/cosmos.staking.v1beta1.MsgDelegate");
+      expect(msgType).toEqual("my-override/MsgDelegate");
+    });
+
     it("throws for unknown type url", () => {
       expect(() => new AminoTypes().toAmino("/xxx.Unknown")).toThrowError(
         /Type URL does not exist in the Amino message type register./i,
@@ -19,6 +31,20 @@ describe("AminoTypes", () => {
     it("works for known type url", () => {
       const msgUrl = new AminoTypes().fromAmino("cosmos-sdk/MsgDelegate");
       expect(msgUrl).toEqual("/cosmos.staking.v1beta1.MsgDelegate");
+    });
+
+    it("works with custom type url", () => {
+      const msgType = new AminoTypes({ "/my.CustomType": "my-sdk/CustomType" }).fromAmino(
+        "my-sdk/CustomType",
+      );
+      expect(msgType).toEqual("/my.CustomType");
+    });
+
+    it("works with overridden type url", () => {
+      const msgType = new AminoTypes({
+        "/my.OverrideType": "cosmos-sdk/MsgDelegate",
+      }).fromAmino("cosmos-sdk/MsgDelegate");
+      expect(msgType).toEqual("/my.OverrideType");
     });
 
     it("throws for unknown type url", () => {
