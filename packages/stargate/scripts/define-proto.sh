6f005798b7c95a770584f161cd690444b2da7eed
willclarktech <willclarktech@users.noreply.github.com>
2021-01-20 15:38:10 +0000

    stargate: Update proto scripts

--- packages/stargate/scripts/define-proto.sh
+++ packages/stargate/scripts/define-proto.sh
@@ -2,15 +2,45 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-TMP_DIR="./tmp"
-JS_SOURCE_FILE="$TMP_DIR/codecimpl.js"
-DEFINITIONS_FILE="$TMP_DIR/codecimpl.d.ts"
-OUTPUT_DIR="./src/codec/generated/"
+ROOT_PROTO_DIR="./proto/cosmos/cosmos-sdk"
+COSMOS_PROTO_DIR="$ROOT_PROTO_DIR/proto"
+THIRD_PARTY_PROTO_DIR="$ROOT_PROTO_DIR/third_party/proto"
+OUT_DIR="./src/codec/"
 
-yarn pbts "$JS_SOURCE_FILE" -o "$DEFINITIONS_FILE"
-# Remove comments after using them for the .d.ts
-# Note "When input files are specified on the command line, tsconfig.json files are ignored." (https://www.typescriptlang.org/docs/handbook/tsconfig-json.html)
-yarn tsc --removeComments --target es2017 --module commonjs --outDir "$OUTPUT_DIR" --allowJs "$JS_SOURCE_FILE"
+mkdir -p "$OUT_DIR"
 
-cp "$DEFINITIONS_FILE" "$OUTPUT_DIR"
-rm "$DEFINITIONS_FILE" "$JS_SOURCE_FILE"
+protoc \
+  --plugin="$(yarn bin protoc-gen-ts_proto)" \
+  --ts_proto_out="$OUT_DIR" \
+  --proto_path="$COSMOS_PROTO_DIR" \
+  --proto_path="$THIRD_PARTY_PROTO_DIR" \
+  --ts_proto_opt="forceLong=long,useOptionals=true" \
+  "$COSMOS_PROTO_DIR/cosmos/auth/v1beta1/auth.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/auth/v1beta1/query.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/bank/v1beta1/bank.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/bank/v1beta1/query.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/bank/v1beta1/tx.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/base/abci/v1beta1/abci.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/base/query/v1beta1/pagination.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/base/v1beta1/coin.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/crypto/multisig/v1beta1/multisig.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/crypto/secp256k1/keys.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/staking/v1beta1/staking.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/staking/v1beta1/tx.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/tx/signing/v1beta1/signing.proto" \
+  "$COSMOS_PROTO_DIR/cosmos/tx/v1beta1/tx.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/channel/v1/channel.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/channel/v1/query.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/client/v1/client.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/commitment/v1/commitment.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/connection/v1/connection.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/connection/v1/query.proto" \
+  "$THIRD_PARTY_PROTO_DIR/confio/proofs.proto" \
+  "$THIRD_PARTY_PROTO_DIR/tendermint/abci/types.proto" \
+  "$THIRD_PARTY_PROTO_DIR/tendermint/crypto/keys.proto" \
+  "$THIRD_PARTY_PROTO_DIR/tendermint/crypto/proof.proto" \
+  "$THIRD_PARTY_PROTO_DIR/tendermint/libs/bits/types.proto" \
+  "$THIRD_PARTY_PROTO_DIR/tendermint/types/params.proto" \
+  "$THIRD_PARTY_PROTO_DIR/tendermint/types/types.proto" \
+  "$THIRD_PARTY_PROTO_DIR/tendermint/types/validator.proto" \
+  "$THIRD_PARTY_PROTO_DIR/tendermint/version/types.proto"
