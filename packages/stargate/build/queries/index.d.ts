69bf3d1e4edc1e271ffaa39aed3a100c8cfa89a1
willclarktech <willclarktech@users.noreply.github.com>
2021-02-04 13:38:15 +0000

    stargate: Remove built type definitions from VCS

--- packages/stargate/build/queries/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { QueryClient } from "./queryclient";
-export { AuthExtension, setupAuthExtension } from "./auth";
-export { BankExtension, setupBankExtension } from "./bank";
-export { DistributionExtension, setupDistributionExtension } from "./distribution";
-export { IbcExtension, setupIbcExtension } from "./ibc";
-export { setupStakingExtension, StakingExtension } from "./staking";
-export { createPagination, createRpc, Rpc } from "./utils";
