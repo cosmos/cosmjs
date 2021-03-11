72c6e9c7f1ee41613d084461c141a8122275d6b9
Simon Warta <simon@warta.it>
2021-03-11 15:37:32 +0100

    Exclude workers from documentation

--- packages/json-rpc/typedoc.js
+++ packages/json-rpc/typedoc.js
@@ -3,7 +3,7 @@ const packageJson = require("./package.json");
 module.exports = {
   entryPoints: ["./src"],
   out: "docs",
-  exclude: "**/*.spec.ts",
+  exclude: ["**/*.spec.ts", "**/*.worker.ts"],
   name: `${packageJson.name} Documentation`,
   readme: "README.md",
   excludeExternals: true,
