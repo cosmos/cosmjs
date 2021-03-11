bce47e21f2739584cc5a1aeaf754c3c5bb5d9a70
Simon Warta <simon@warta.it>
2021-03-11 15:36:18 +0100

    Upgrade typedoc to 0.20

--- packages/ledger-amino/typedoc.js
+++ packages/ledger-amino/typedoc.js
@@ -1,13 +1,11 @@
 const packageJson = require("./package.json");
 
 module.exports = {
-  inputFiles: ["./src"],
+  entryPoints: ["./src"],
   out: "docs",
   exclude: ["**/*.spec.ts", "./src/demo"],
   name: `${packageJson.name} Documentation`,
   readme: "README.md",
-  mode: "file",
   excludeExternals: true,
-  excludeNotExported: true,
   excludePrivate: true,
 };
