3db78cd5f77ab0f1dbdd7c3efc355940d01189a1
willclarktech <willclarktech@users.noreply.github.com>
2021-03-24 18:07:39 +0100

    proto-signing: Use paths from amino

--- packages/proto-signing/src/index.ts
+++ packages/proto-signing/src/index.ts
@@ -10,7 +10,6 @@ export {
 } from "./registry";
 export { DirectSecp256k1HdWallet } from "./directsecp256k1hdwallet";
 export { DirectSecp256k1Wallet } from "./directsecp256k1wallet";
-export { makeCosmoshubPath } from "./paths";
 export { decodePubkey, encodePubkey } from "./pubkey";
 export {
   AccountData,
