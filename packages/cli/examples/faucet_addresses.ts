580b417b70a8ea37e466766aaf40dd5674b8410e
willclarktech <willclarktech@users.noreply.github.com>
2021-04-20 17:53:33 +0200

    cli: Update examples for new Secp256k1HdWallet options

--- packages/cli/examples/faucet_addresses.ts
+++ packages/cli/examples/faucet_addresses.ts
@@ -4,9 +4,11 @@ import { toBase64 } from "@cosmjs/encoding";
 const mnemonic =
   "economy stock theory fatal elder harbor betray wasp final emotion task crumble siren bottom lizard educate guess current outdoor pair theory focus wife stone";
 
-for (let i of [0, 1, 2, 3, 4]) {
-  const wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { hdPath: makeCosmoshubPath(i) });
-  const [{ address, pubkey }] = await wallet.getAccounts();
+const accountNumbers = [0, 1, 2, 3, 4];
+const hdPaths = accountNumbers.map(makeCosmoshubPath);
+const wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, { hdPaths: hdPaths });
+const accounts = await wallet.getAccounts();
+accounts.forEach(({ address, pubkey }, i) => {
   console.info(`Address ${i}: ${address}`);
   console.info(`Pubkey ${i}: ${toBase64(pubkey)}`);
-}
+});
