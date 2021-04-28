928b12c72544c2e11fcff2ff36b57e6e762ed8de
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-04-28 20:17:15 +0200

    Add download scripts for wasm contracts

---
+++ scripts/wasmd/contracts/download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This must get from 2-3 different repos, fix the versions here:
+
+COSMWASM_VERSION="v0.14.0-beta5"
+PLUS_VERSION="v0.6.0-beta2"
+
+curl -L -O "https://github.com/CosmWasm/cosmwasm/releases/download/${COSMWASM_VERSION}/hackatom.wasm"
+
+curl -L -O "https://github.com/CosmWasm/cosmwasm-plus/releases/download/${PLUS_VERSION}/cw1_subkeys.wasm"
+curl -L -O "https://github.com/CosmWasm/cosmwasm-plus/releases/download/${PLUS_VERSION}/cw3_fixed_multisig.wasm"
+
+sha256sum *.wasm > checksums.sha256
