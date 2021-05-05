df01734ba29d243cf0cd9030428d6e79e6fd5386
bartmacbartek <bartekconfio@gmail.com>
2021-05-06 00:12:44 +0200

    Remove object destructuring for hdPaths, prefix and seed

--- packages/proto-signing/src/directsecp256k1hdwallet.ts
+++ packages/proto-signing/src/directsecp256k1hdwallet.ts
@@ -91,13 +91,10 @@ export class DirectSecp256k1HdWallet implements OfflineDirectSigner {
   private readonly accounts: readonly Secp256k1Derivation[];
 
   protected constructor(mnemonic: EnglishMnemonic, options: DirectSecp256k1HdWalletConstructorOptions) {
-    const { seed, hdPaths, prefix } = {
-      seed: options.seed,
-      prefix: options.prefix ?? defaultOptions.prefix,
-      hdPaths: options.hdPaths ?? defaultOptions.hdPaths,
-    };
+    const prefix = options.prefix ?? defaultOptions.prefix;
+    const hdPaths = options.hdPaths ?? defaultOptions.hdPaths;
     this.secret = mnemonic;
-    this.seed = seed;
+    this.seed = options.seed;
     this.accounts = hdPaths.map((hdPath) => ({
       hdPath: hdPath,
       prefix: prefix,
