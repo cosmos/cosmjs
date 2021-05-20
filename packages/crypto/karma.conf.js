9401584a72cedabf2bba432ab9f7383fb4f2c1cc
willclarktech <willclarktech@users.noreply.github.com>
2021-05-20 12:21:27 +0200

    crypto: Standardise log level in karma config

--- packages/crypto/karma.conf.js
+++ packages/crypto/karma.conf.js
@@ -37,10 +37,7 @@ module.exports = function (config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-
-    // Use debug until Firefox starting issues on Travis are understood
-    // https://travis-ci.com/iov-one/iov-core/jobs/174888518
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
