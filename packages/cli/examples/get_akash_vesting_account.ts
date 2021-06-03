3116590bd79f8e2ce7a8df0c75cf226594123d18
willclarktech <willclarktech@users.noreply.github.com>
2021-06-03 13:39:32 +0200

    cli: Fix example

--- packages/cli/examples/get_akash_vesting_account.ts
+++ packages/cli/examples/get_akash_vesting_account.ts
@@ -7,5 +7,5 @@ const tmClient = await Tendermint34Client.connect("http://rpc.akash.forbole.com:
 const client = QueryClient.withExtensions(tmClient, setupAuthExtension);
 
 // Arbitrary entry from https://raw.githubusercontent.com/ovrclk/net/24ddbb427/mainnet/genesis.json
-const account = await client.auth.unverified.account("akash1qy0vur3fl2ucztpzcrfea7mc8jwz8xjmvq7qvy");
+const account = await client.auth.account("akash1qy0vur3fl2ucztpzcrfea7mc8jwz8xjmvq7qvy");
 console.log(Any.toJSON(account));
