a172327a2ab53159dc6104bcf723c7bd10d3fcb2
willclarktech <willclarktech@users.noreply.github.com>
2021-06-08 17:04:32 +0200

    tendermint-rpc: Remove eslint overrides

--- packages/tendermint-rpc/src/tendermint33/adaptor/index.ts
+++ packages/tendermint-rpc/src/tendermint33/adaptor/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import { hashBlock, hashTx } from "../hasher";
 import { Params } from "./requests";
 import { Responses } from "./responses";
