23dfbe93254ae4e52331623f55b998ec621e1aca
willclarktech <willclarktech@users.noreply.github.com>
2021-06-08 16:09:01 +0200

    tendermint-rpc: Rename 0.33 client files

--- packages/tendermint-rpc/src/tendermint33/index.ts
+++ packages/tendermint-rpc/src/tendermint33/index.ts
@@ -1,6 +1,6 @@
 export { adaptor33, adaptor34 } from "./adaptors";
 export { Adaptor } from "./adaptor";
-export { Client } from "./client";
+export { Client } from "./tendermint33client";
 export {
   AbciInfoRequest,
   AbciQueryParams,
