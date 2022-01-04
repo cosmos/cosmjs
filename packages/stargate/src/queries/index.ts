576bf2a5a235052bb2b60ff70a53d3405a73fef0
Simon Warta <simon@warta.it>
2022-01-04 22:49:13 +0100

    Add MintExtension for queries

--- packages/stargate/src/queries/index.ts
+++ packages/stargate/src/queries/index.ts
@@ -9,6 +9,7 @@ export { BankExtension, setupBankExtension } from "./bank";
 export { DistributionExtension, setupDistributionExtension } from "./distribution";
 export { GovExtension, GovParamsType, GovProposalId, setupGovExtension } from "./gov";
 export { IbcExtension, setupIbcExtension } from "./ibc";
+export { MintExtension, MintParams, setupMintExtension } from "./mint";
 export { setupStakingExtension, StakingExtension } from "./staking";
 export { setupTxExtension, TxExtension } from "./tx";
 export {
