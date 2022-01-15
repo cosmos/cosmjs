9775a930c0b72866a555bc0b3c56e7f90b0b0bf9
daoauth <57783762+daoauth@users.noreply.github.com>
2022-01-15 13:43:36 +0900

    Update stargateclient.ts

--- packages/stargate/src/stargateclient.ts
+++ packages/stargate/src/stargateclient.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { toHex } from "@cosmjs/encoding";
 import { Uint53 } from "@cosmjs/math";
-import { Tendermint34Client, toRfc3339WithNanoseconds, RpcClient } from "@cosmjs/tendermint-rpc";
+import { RpcClient, Tendermint34Client, toRfc3339WithNanoseconds } from "@cosmjs/tendermint-rpc";
 import { sleep } from "@cosmjs/utils";
 import { MsgData } from "cosmjs-types/cosmos/base/abci/v1beta1/abci";
 import { Coin } from "cosmjs-types/cosmos/base/v1beta1/coin";
