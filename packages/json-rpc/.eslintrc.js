c3f062dfb11d39e5619a551b25783b07e18d6c76
Simon Warta <simon@warta.it>
2022-01-25 16:50:03 +0100

    Disable @typescript-eslint/explicit-module-boundary-types for .js files

--- packages/json-rpc/.eslintrc.js
+++ packages/json-rpc/.eslintrc.js
@@ -81,6 +81,7 @@ module.exports = {
         "@typescript-eslint/no-var-requires": "off",
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-member-accessibility": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
       },
     },
     {
