186bad56f9ffe4615ae30ce5570b8fc07d813a43
Milan Steiner <steiner.milan@hotmail.com>
2022-02-22 11:11:59 +0100

    Rename property ledger to connector

--- packages/ledger-amino/src/ledgersigner.ts
+++ packages/ledger-amino/src/ledgersigner.ts
@@ -13,22 +13,22 @@ import Transport from "@ledgerhq/hw-transport";
 import { AddressAndPubkey, LedgerConnector, LedgerConnectorOptions } from "./ledgerconnector";
 
 export class LedgerSigner implements OfflineAminoSigner {
-  private readonly ledger: LedgerConnector;
+  private readonly connector: LedgerConnector;
   private readonly hdPaths: readonly HdPath[];
   private accounts?: readonly AccountData[];
 
   public constructor(transport: Transport, options: LedgerConnectorOptions = {}) {
     this.hdPaths = options.hdPaths || [makeCosmoshubPath(0)];
-    this.ledger = new LedgerConnector(transport, options);
+    this.connector = new LedgerConnector(transport, options);
   }
 
   public async getAccounts(): Promise<readonly AccountData[]> {
     if (!this.accounts) {
-      const pubkeys = await this.ledger.getPubkeys();
+      const pubkeys = await this.connector.getPubkeys();
       this.accounts = await Promise.all(
         pubkeys.map(async (pubkey) => ({
           algo: "secp256k1" as const,
-          address: await this.ledger.getCosmosAddress(pubkey),
+          address: await this.connector.getCosmosAddress(pubkey),
           pubkey: pubkey,
         })),
       );
@@ -46,7 +46,7 @@ export class LedgerSigner implements OfflineAminoSigner {
    * @param path The HD path to show the address for. If unset, this is the first account.
    */
   public async showAddress(path?: HdPath): Promise<AddressAndPubkey> {
-    return this.ledger.showAddress(path);
+    return this.connector.showAddress(path);
   }
 
   public async signAmino(signerAddress: string, signDoc: StdSignDoc): Promise<AminoSignResponse> {
@@ -60,7 +60,7 @@ export class LedgerSigner implements OfflineAminoSigner {
     const message = serializeSignDoc(signDoc);
     const accountForAddress = accounts[accountIndex];
     const hdPath = this.hdPaths[accountIndex];
-    const signature = await this.ledger.sign(message, hdPath);
+    const signature = await this.connector.sign(message, hdPath);
     return {
       signed: signDoc,
       signature: encodeSecp256k1Signature(accountForAddress.pubkey, signature),
