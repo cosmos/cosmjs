42dbcfb46dd19040427a60ffe73e4f59090e379c
Simon Warta <simon@warta.it>
2022-03-08 11:42:07 +0100

    Implement formatDecimal and formatCoin

--- packages/proto-signing/src/textual/valuerenderers.spec.ts
+++ packages/proto-signing/src/textual/valuerenderers.spec.ts
@@ -1,4 +1,12 @@
-import { formatInteger } from "./valuerenderers";
+import { DenomUnit } from "cosmjs-types/cosmos/bank/v1beta1/bank";
+import { Coin } from "cosmjs-types/cosmos/base/v1beta1/coin";
+
+import coins from "../testdata/coins.json";
+import decimals from "../testdata/decimals.json";
+import integers from "../testdata/integers.json";
+import { formatCoin, formatDecimal, formatInteger } from "./valuerenderers";
+
+type TestDataCoins = Array<[Coin, Pick<DenomUnit, "denom" | "exponent">, string]>;
 
 describe("valuerenderers", () => {
   describe("formatInteger", () => {
@@ -10,6 +18,34 @@ describe("valuerenderers", () => {
       expect(formatInteger("12345")).toEqual("12'345");
       expect(formatInteger("123456")).toEqual("123'456");
       expect(formatInteger("1234567")).toEqual("1'234'567");
+
+      for (const [input, expected] of integers) {
+        expect(formatInteger(input)).withContext(`Input '${input}'`).toEqual(expected);
+      }
+    });
+  });
+
+  describe("formatDecimal", () => {
+    it("works", () => {
+      expect(formatDecimal("1234.5678")).toEqual("1'234.5678");
+
+      for (const [input, expected] of decimals) {
+        expect(formatDecimal(input)).withContext(`Input '${input}'`).toEqual(expected);
+      }
+    });
+  });
+
+  describe("formatCoin", () => {
+    it("works", () => {
+      expect(formatCoin({ amount: "1", denom: "ucosm" }, { denom: "COSM", exponent: 6 })).toEqual(
+        "0.000001 COSM",
+      );
+
+      for (const [coin, unit, expected] of coins as TestDataCoins) {
+        expect(formatCoin(coin, unit))
+          .withContext(`Input '${JSON.stringify(coin)}'`)
+          .toEqual(expected);
+      }
     });
   });
 });
--- packages/proto-signing/src/textual/valuerenderers.ts
+++ packages/proto-signing/src/textual/valuerenderers.ts
@@ -1,5 +1,19 @@
 import { Decimal } from "@cosmjs/math";
+import { DenomUnit } from "cosmjs-types/cosmos/bank/v1beta1/bank";
+import { Coin } from "cosmjs-types/cosmos/base/v1beta1/coin";
 
 export function formatInteger(input: string): string {
   return Decimal.fromAtomics(input, 0).toString(".", "'");
+}
+
+export function formatDecimal(input: string): string {
+  let out = Decimal.fromUserInput(input, 18).toString(".", "'");
+  if (out.indexOf(".") === -1) out += ".0";
+  return out;
+}
+
+export function formatCoin(input: Coin, unit: Pick<DenomUnit, "denom" | "exponent">): string {
+  let out = Decimal.fromAtomics(input.amount, unit.exponent).toString(".", "'");
+  if (unit.exponent !== 0 && out.indexOf(".") === -1) out += ".0";
+  return out + " " + unit.denom;
 }
