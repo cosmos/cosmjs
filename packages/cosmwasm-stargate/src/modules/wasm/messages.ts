458d4d03cd3a3b8d8942978acb2c8874fdf0354f
Simon Warta <simon@warta.it>
2022-03-08 21:49:26 +0100

    Add importWasmMessages

--- packages/cosmwasm-stargate/src/modules/wasm/messages.ts
+++ packages/cosmwasm-stargate/src/modules/wasm/messages.ts
@@ -1,4 +1,6 @@
+import { toUtf8 } from "@cosmjs/encoding";
 import { EncodeObject, GeneratedType } from "@cosmjs/proto-signing";
+import { isNonNullObject } from "@cosmjs/utils";
 import {
   MsgClearAdmin,
   MsgExecuteContract,
@@ -7,6 +9,7 @@ import {
   MsgStoreCode,
   MsgUpdateAdmin,
 } from "cosmjs-types/cosmwasm/wasm/v1/tx";
+import Long from "long";
 
 export const wasmTypes: ReadonlyArray<[string, GeneratedType]> = [
   ["/cosmwasm.wasm.v1.MsgClearAdmin", MsgClearAdmin],
@@ -73,4 +76,59 @@ export interface MsgExecuteContractEncodeObject extends EncodeObject {
 
 export function isMsgExecuteEncodeObject(object: EncodeObject): object is MsgExecuteContractEncodeObject {
   return (object as MsgExecuteContractEncodeObject).typeUrl === "/cosmwasm.wasm.v1.MsgExecuteContract";
+}
+
+/**
+ * Takes a JSON representation of the Go implementation and imports it into CosmJS types
+ *
+ * The Go JSON and the ts-proto JSON are not compatible, i.e. we have to implement every
+ * message individually.
+ */
+export function importWasmMessages(
+  json: string,
+): Array<MsgExecuteContractEncodeObject | MsgInstantiateContractEncodeObject> {
+  const doc = JSON.parse(json);
+  if (!Array.isArray(doc)) throw new Error("Array expected");
+  const out = [];
+  for (const element of doc) {
+    if (!isNonNullObject(element)) throw new Error("Element must be an object");
+    if (!("@type" in element)) throw new Error("Element is missing a @type field");
+    const typeUrl = (element as any)["@type"];
+    if (typeof typeUrl !== "string") throw new Error("Element's @type fiels must be a string");
+    switch (typeUrl) {
+      case "/cosmwasm.wasm.v1.MsgExecuteContract": {
+        // console.log(element);
+        const msg: MsgExecuteContractEncodeObject = {
+          typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+          value: MsgExecuteContract.fromPartial({
+            sender: (element as any).sender,
+            contract: (element as any).contract,
+            msg: toUtf8(JSON.stringify((element as any).msg)),
+            funds: (element as any).funds,
+          }),
+        };
+        out.push(msg);
+        break;
+      }
+      case "/cosmwasm.wasm.v1.MsgInstantiateContract": {
+        // console.log(element);
+        const msg: MsgInstantiateContractEncodeObject = {
+          typeUrl: "/cosmwasm.wasm.v1.MsgInstantiateContract",
+          value: MsgInstantiateContract.fromPartial({
+            sender: (element as any).sender,
+            admin: (element as any).admin,
+            codeId: Long.fromString((element as any).code_id, true, 10),
+            label: (element as any).label,
+            msg: toUtf8(JSON.stringify((element as any).msg)),
+            funds: (element as any).funds,
+          }),
+        };
+        out.push(msg);
+        break;
+      }
+      default:
+        throw new Error(`Unsupported message type '${typeUrl}' found.`);
+    }
+  }
+  return out;
 }
