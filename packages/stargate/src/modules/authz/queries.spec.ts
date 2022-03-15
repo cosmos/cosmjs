0f67ba140c52519b0caf651461d845428f8994f4
Milan Steiner <steiner.milan@hotmail.com>
2022-03-15 16:23:20 +0100

    Fix Typo

--- packages/stargate/src/modules/authz/queries.spec.ts
+++ packages/stargate/src/modules/authz/queries.spec.ts
@@ -2,6 +2,7 @@ import { makeCosmoshubPath } from "@cosmjs/amino";
 import { coins, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
 import { sleep } from "@cosmjs/utils";
+import { GenericAuthorization } from "cosmjs-types/cosmos/authz/v1beta1/authz";
 
 import { QueryClient } from "../../queryclient";
 import { SigningStargateClient } from "../../signingstargateclient";
