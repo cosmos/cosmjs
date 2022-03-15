833205dd02591ea457bf8a1e9a2b90767ced4ad1
Simon Warta <simon@warta.it>
2022-03-15 22:49:49 +0100

    Add waiting time to timeout error message

--- packages/stargate/src/stargateclient.ts
+++ packages/stargate/src/stargateclient.ts
@@ -350,7 +350,9 @@ export class StargateClient {
     const pollForTx = async (txId: string): Promise<DeliverTxResponse> => {
       if (timedOut) {
         throw new TimeoutError(
-          `Transaction with ID ${txId} was submitted but was not yet found on the chain. You might want to check later.`,
+          `Transaction with ID ${txId} was submitted but was not yet found on the chain. You might want to check later. There was a wait of ${
+            timeoutMs / 10000
+          } seconds. Approx. ${timeoutMs / pollIntervalMs} blocks were created during this time.`,
           txId,
         );
       }
