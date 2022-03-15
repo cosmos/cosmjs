51a8fddbd456fc5f444e7b4a42663d434a444f11
Milan Steiner <69144826+msteiner96@users.noreply.github.com>
2022-03-15 19:25:20 +0100

    Update stargateclient.ts

--- packages/stargate/src/stargateclient.ts
+++ packages/stargate/src/stargateclient.ts
@@ -352,7 +352,7 @@ export class StargateClient {
         throw new TimeoutError(
           `Transaction with ID ${txId} was submitted but was not yet found on the chain. You might want to check later. There was a wait of ${
             timeoutMs / 10000
-          } seconds. Approx. ${timeoutMs / pollIntervalMs} blocks were created during this time.`,
+          } seconds.`,
           txId,
         );
       }
