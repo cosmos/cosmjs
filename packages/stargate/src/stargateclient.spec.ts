99ad9e4289604fc1a64a814c3f3f177d0b67b179
Youngjoon Lee <taxihighway@gmail.com>
2022-08-16 22:19:54 +0900

    fix tests

--- packages/stargate/src/stargateclient.spec.ts
+++ packages/stargate/src/stargateclient.spec.ts
@@ -381,7 +381,12 @@ describe("StargateClient", () => {
       const { gasUsed, rawLog, data, transactionHash } = txResult;
       expect(gasUsed).toBeGreaterThan(0);
       expect(rawLog).toMatch(/{"key":"amount","value":"1234567ucosm"}/);
-      expect(data).toEqual([{ msgType: "/cosmos.bank.v1beta1.MsgSend", data: new Uint8Array() }]); // MsgSendResponse is empty
+      // MsgSendResponse is empty
+      if (simapp44Enabled()) {
+        expect(data).toEqual([{ msgType: "/cosmos.bank.v1beta1.MsgSend", data: new Uint8Array() }]);
+      } else {
+        expect(data).toBeUndefined();
+      }
       expect(transactionHash).toMatch(/^[0-9A-F]{64}$/);
 
       client.disconnect();
