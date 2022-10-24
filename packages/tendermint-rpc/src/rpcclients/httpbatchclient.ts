21695f2932fd9ef4dcc3ea639a714f2c7d7f59a9
codehans <94654388+codehans@users.noreply.github.com>
2022-10-24 10:45:56 +0100

    check falsy values

--- packages/tendermint-rpc/src/rpcclients/httpbatchclient.ts
+++ packages/tendermint-rpc/src/rpcclients/httpbatchclient.ts
@@ -58,7 +58,8 @@ export class HttpBatchClient implements RpcClient {
   }
 
   private validate(): void {
-    if (this.options.batchSizeLimit < 1) throw new Error("batchSizeLimit < 1");
+    if (!this.options.batchSizeLimit || this.options.batchSizeLimit < 1)
+      throw new Error("batchSizeLimit < 1");
   }
 
   private async tick(): Promise<void> {
