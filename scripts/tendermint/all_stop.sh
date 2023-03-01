a14c9eb563107c38a94999df5262af6e320fec75
Simon Warta <simon@warta.it>
2023-03-01 17:23:38 +0100

    Switch to cometbft/cometbft:v0.37.0-rc3

--- scripts/tendermint/all_stop.sh
+++ scripts/tendermint/all_stop.sh
@@ -2,14 +2,9 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-declare -a TM_VERSIONS
-TM_VERSIONS[34]=v0.34.19
-TM_VERSIONS[35]=v0.35.6
-TM_VERSIONS[37]=v0.37.0-rc2
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-for KEY in "${!TM_VERSIONS[@]}"; do
+for KEY in 34 35 37; do
   export TENDERMINT_NAME="tendermint-$KEY"
 
   echo "Stopping $TENDERMINT_NAME ..."
