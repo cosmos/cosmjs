3ad87362ded673b6fa97150dc75383d82d8f0c41
Simon Warta <simon@warta.it>
2023-03-01 13:50:30 +0100

    Add Tendermint 0.37 docker

--- scripts/tendermint/all_start.sh
+++ scripts/tendermint/all_start.sh
@@ -6,6 +6,7 @@ command -v shellcheck >/dev/null && shellcheck "$0"
 declare -a TM_VERSIONS
 TM_VERSIONS[34]=v0.34.19
 TM_VERSIONS[35]=v0.35.6
+TM_VERSIONS[37]=v0.37.0-rc2
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
