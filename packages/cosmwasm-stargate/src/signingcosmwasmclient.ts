3ce9005a2ba31f35e1bb51aee2e5f886d0d1e930
jinseong.cho <jinseong.cho@linecorp.com>
2023-04-04 13:49:03 +0900

    feat: add instantiate2 broadcast

--- packages/cosmwasm-stargate/src/signingcosmwasmclient.ts
+++ packages/cosmwasm-stargate/src/signingcosmwasmclient.ts
@@ -41,6 +41,7 @@ import {
   MsgClearAdmin,
   MsgExecuteContract,
   MsgInstantiateContract,
+  MsgInstantiateContract2,
   MsgMigrateContract,
   MsgStoreCode,
   MsgUpdateAdmin,
@@ -60,6 +61,7 @@ import {
   MsgUpdateAdminEncodeObject,
   wasmTypes,
 } from "./modules";
+import { MsgInstantiateContract2EncodeObject } from "./modules/wasm/messages";
 
 export interface UploadResult {
   /** Size of the original wasm code in bytes */
@@ -101,6 +103,38 @@ export interface InstantiateOptions {
    * Caution: an admin has the privilege to upgrade a contract. If this is not desired, do not set this value.
    */
   readonly admin?: string;
+}
+
+/**
+ * The options of an .instantiate() call.
+ * All properties are optional.
+ */
+export interface Instantiate2Options {
+  readonly memo?: string;
+  /**
+   * The funds that are transferred from the sender to the newly created contract.
+   * The funds are transferred as part of the message execution after the contract address is
+   * created and before the instantiation message is executed by the contract.
+   *
+   * Only native tokens are supported.
+   */
+  readonly funds?: readonly Coin[];
+  /**
+   * A bech32 encoded address of an admin account.
+   * Caution: an admin has the privilege to upgrade a contract. If this is not desired, do not set this value.
+   */
+  readonly admin?: string;
+
+  /**
+   * salt is an arbitrary value provided by the sender. Size can be 1 to 64.
+   */
+  readonly salt?: Uint8Array;
+
+  /**
+   * FixMsg include the msg value into the hash for the predictable address.
+   * Default is false
+   */
+  readonly fixMsg?: boolean;
 }
 
 export interface InstantiateResult {
@@ -319,6 +353,44 @@ export class SigningCosmWasmClient extends CosmWasmClient {
       }),
     };
     const result = await this.signAndBroadcast(senderAddress, [instantiateContractMsg], fee, options.memo);
+    if (isDeliverTxFailure(result)) {
+      throw new Error(createDeliverTxResponseErrorMessage(result));
+    }
+    const parsedLogs = logs.parseRawLog(result.rawLog);
+    const contractAddressAttr = logs.findAttribute(parsedLogs, "instantiate", "_contract_address");
+    return {
+      contractAddress: contractAddressAttr.value,
+      logs: parsedLogs,
+      height: result.height,
+      transactionHash: result.transactionHash,
+      events: result.events,
+      gasWanted: result.gasWanted,
+      gasUsed: result.gasUsed,
+    };
+  }
+
+  public async instantiate2(
+    senderAddress: string,
+    codeId: number,
+    msg: Record<string, unknown>,
+    label: string,
+    fee: StdFee | "auto" | number,
+    options: Instantiate2Options = {},
+  ): Promise<InstantiateResult> {
+    const instantiateContract2Msg: MsgInstantiateContract2EncodeObject = {
+      typeUrl: "/cosmwasm.wasm.v1.MsgInstantiateContract2",
+      value: MsgInstantiateContract2.fromPartial({
+        sender: senderAddress,
+        codeId: Long.fromString(new Uint53(codeId).toString()),
+        label: label,
+        msg: toUtf8(JSON.stringify(msg)),
+        funds: [...(options.funds || [])],
+        admin: options.admin,
+        salt: options.salt,
+        fixMsg: options.fixMsg,
+      }),
+    };
+    const result = await this.signAndBroadcast(senderAddress, [instantiateContract2Msg], fee, options.memo);
     if (isDeliverTxFailure(result)) {
       throw new Error(createDeliverTxResponseErrorMessage(result));
     }
