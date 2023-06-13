f3351aba55e14bcf6d7575acb7f83b340ab20465
Simon Warta <simon@warta.it>
2023-06-13 08:33:45 +0200

    Upgrade wasmd to 0.40.0

--- scripts/wasmd/init.sh
+++ scripts/wasmd/init.sh
@@ -2,12 +2,12 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-echo "Waiting for blockchain and LCD server to be available ..."
-timeout 60 bash -c "until curl -s http://localhost:1319/node_info > /dev/null; do sleep 0.5; done"
+echo "Waiting for blockchain and Tendermint RPC server to be available ..."
+timeout 60 bash -c "until curl -s http://localhost:26659/health > /dev/null; do sleep 0.5; done"
 # The chain is unreliable in the first second of its existence (https://gist.github.com/webmaster128/8175692d4af5e6c572fddda7a9ef437c)
 sleep 2
 echo "Waiting for height to be >= 1 ..."
-timeout 20 bash -c "until [ \"\$( curl -s http://localhost:1319/blocks/latest | jq -r '.block.header.height // 0' )\" -ge 1 ]; do sleep 0.5; done"
+timeout 20 bash -c "until [ \"\$( curl -s http://localhost:26659/status | jq -r '.result.sync_info.latest_block_height // 0' )\" -ge 1 ]; do sleep 0.5; done"
 echo "Okay, thank you for your patience."
 
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
