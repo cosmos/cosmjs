d8412cf90a3cd975064c124f7377b154c27b9832
Simon Warta <simon@warta.it>
2023-06-13 14:55:15 +0200

    Remove not_supported_by_chain for authz and feegrant messages

--- packages/stargate/src/modules/authz/aminomessages.ts
+++ packages/stargate/src/modules/authz/aminomessages.ts
@@ -2,8 +2,12 @@ import { AminoConverters } from "../../aminotypes";
 
 export function createAuthzAminoConverters(): AminoConverters {
   return {
-    "/cosmos.authz.v1beta1.MsgGrant": "not_supported_by_chain",
-    "/cosmos.authz.v1beta1.MsgExec": "not_supported_by_chain",
-    "/cosmos.authz.v1beta1.MsgRevoke": "not_supported_by_chain",
+    // For Cosmos SDK < 0.46 the Amino JSON codec was broken on chain and thus inaccessible.
+    // Now this can be implemented for 0.46+ chains, see
+    // https://github.com/cosmos/cosmjs/issues/1092
+    //
+    // "/cosmos.authz.v1beta1.MsgGrant": IMPLEMENT ME,
+    // "/cosmos.authz.v1beta1.MsgExec": IMPLEMENT ME,
+    // "/cosmos.authz.v1beta1.MsgRevoke": IMPLEMENT ME,
   };
 }
