5578acd4c401246202e95244cf2832e928c4cd5f
Clockwork <alex@clockwork.gr>
2023-11-30 08:52:15 +0200

    fix: Ensure RFC dates between years 0001 and 0099 are parsed correctly

--- packages/encoding/src/rfc3339.ts
+++ packages/encoding/src/rfc3339.ts
@@ -40,7 +40,12 @@ export function fromRfc3339(str: string): Date {
 
   const tzOffset = tzOffsetSign * (tzOffsetHours * 60 + tzOffsetMinutes) * 60; // seconds
 
-  const timestamp = Date.UTC(year, month - 1, day, hour, minute, second, milliSeconds) - tzOffset * 1000;
+  let timestamp = Date.UTC(year, month - 1, day, hour, minute, second, milliSeconds);
+
+  // Date.UTC maps year 0-99 to 1900-1999. Ensure the correct year is set and THEN apply the offset
+  const date = new Date(timestamp);
+  timestamp = date.setUTCFullYear(year) - tzOffset * 1000;
+
   return new Date(timestamp);
 }
 
