193369a639a4ac1d220753e16845cbe83cda3d5d
hoangdv2429 <hoangdv2429@gmail.com>
2024-01-13 11:36:08 +0700

    other cases for fromAmino

--- packages/stargate/src/modules/authz/aminomessages.spec.ts
+++ packages/stargate/src/modules/authz/aminomessages.spec.ts
@@ -1,10 +1,9 @@
 import { MsgExec, MsgGrant, MsgRevoke } from "cosmjs-types/cosmos/authz/v1beta1/tx";
 import { SendAuthorization } from "cosmjs-types/cosmos/bank/v1beta1/authz";
-import { AminoConverters } from "src/aminotypes";
 
-import { AminoMsgGrant, createAuthzAminoConverters } from "./aminomessages";
-import { Timestamp } from "cosmjs-types/google/protobuf/timestamp";
+import { AminoMsgExec, AminoMsgGrant, AminoMsgRevoke, createAuthzAminoConverters } from "./aminomessages";
 import { AminoTypes } from "../../aminotypes";
+import { MsgSend } from "cosmjs-types/cosmos/bank/v1beta1/tx";
 
 describe("Authz Amino Converters", () => {
   describe("fromAmino", () => {
@@ -53,10 +52,70 @@ describe("Authz Amino Converters", () => {
       });
     });
 
-    // it("toAmino", () => {});
+    it("work with MsgExec", () => {
+      // Define a sample AminoMsgExec object
+      const msg: AminoMsgExec = {
+        type: "cosmos-sdk/MsgExec",
+        value: {
+          grantee: "cosmos10dyr9899g6t0pelew4nvf4j5c3jcgv0r73qga5",
+          msgs: [
+            {
+              typeUrl: "cosmos-sdk/MsgSend",
+              value: MsgSend.encode({
+                fromAddress: "cosmos1p7v0km9ydt0y9nszlesjy8elzqc4n2v0w4xh2p",
+                toAddress: "cosmos1p7v0km9ydt0y9nszlesjy8elzqc4n2v0w4xh2p",
+                amount: [{ denom: "ustake", amount: "1000000" }],
+              }).finish(),
+            },
+          ],
+        },
+      };
+
+      const msgExec = new AminoTypes(createAuthzAminoConverters()).fromAmino(msg);
+      const expectedValue: MsgExec = {
+        grantee: "cosmos10dyr9899g6t0pelew4nvf4j5c3jcgv0r73qga5",
+        msgs: [
+          {
+            typeUrl: "cosmos-sdk/MsgSend",
+            value: MsgSend.encode({
+              fromAddress: "cosmos1p7v0km9ydt0y9nszlesjy8elzqc4n2v0w4xh2p",
+              toAddress: "cosmos1p7v0km9ydt0y9nszlesjy8elzqc4n2v0w4xh2p",
+              amount: [{ denom: "ustake", amount: "1000000" }],
+            }).finish(),
+          },
+        ],
+      };
+
+      expect(msgExec).toEqual({
+        typeUrl: "/cosmos.authz.v1beta1.MsgExec",
+        value: expectedValue,
+      });
+    });
+
+    it("work with MsgRevoke", () => {
+      // Define a sample AminoMsgExec object
+      const msg: AminoMsgRevoke = {
+        type: "cosmos-sdk/MsgRevoke",
+        value: {
+          granter: "cosmos1p7v0km9ydt0y9nszlesjy8elzqc4n2v0w4xh2p",
+          grantee: "cosmos10dyr9899g6t0pelew4nvf4j5c3jcgv0r73qga5",
+          msg_type_url: "cosmos-sdk/MsgSend",
+        },
+      };
+
+      const msgRevoke = new AminoTypes(createAuthzAminoConverters()).fromAmino(msg);
+      const expectedValue: MsgRevoke = {
+        granter: "cosmos1p7v0km9ydt0y9nszlesjy8elzqc4n2v0w4xh2p",
+        grantee: "cosmos10dyr9899g6t0pelew4nvf4j5c3jcgv0r73qga5",
+        msgTypeUrl: "cosmos-sdk/MsgSend",
+      };
+
+      expect(msgRevoke).toEqual({
+        typeUrl: "/cosmos.authz.v1beta1.MsgRevoke",
+        value: expectedValue,
+      });
+    });
   });
 
-  // describe("MsgExec", () => {});
-
-  // describe("MsgRevoke", () => {});
+  // describe("fromAmino", () => {});
 });
