6629940b5343d38c57a9109b176405c3eb8aaa22
Pynix Wang <pynix.wang@gmail.com>
2024-02-28 15:25:01 +0800

    fix fee

--- scripts/wasmd/send_first.js
+++ scripts/wasmd/send_first.js
@@ -23,7 +23,7 @@ async function main() {
   const wallet = await DirectSecp256k1HdWallet.fromMnemonic(faucet.mnemonic, { prefix: prefix });
   const client = await SigningStargateClient.connectWithSigner(rpcUrl, wallet, { prefix: prefix });
   const recipient = "wasm14qemq0vw6y3gc3u3e0aty2e764u4gs5lndxgyk";
-  const amount = coins(226644, "ucosm");
+  const amount = coins(500000000, "ucosm");
   const fee = calculateFee(100_000, "0.025ucosm");
   const memo = "Ensure chain has my pubkey";
   const sendResult = await client.sendTokens(faucet.address0, recipient, amount, fee, memo);
