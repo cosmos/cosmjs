1c6bb1c96e6732bd7252336f5ed6e166d7cdc211
ducphamle2 <ducphamle212@gmail.com>
2024-08-16 16:42:45 -0700

    feat: add http timeout & allow more data return tx resp

--- packages/tendermint-rpc/src/rpcclients/http.ts
+++ packages/tendermint-rpc/src/rpcclients/http.ts
@@ -40,9 +40,14 @@ export async function http(
   headers: Record<string, string> | undefined,
   request?: any,
 ): Promise<any> {
+  const timeout = Number(
+    //@ts-ignore
+    process.env.HTTP_TIMEOUT || 30000,
+  );
   if (typeof fetch === "function" && !isExperimental(fetch)) {
     const settings = {
       method: method,
+      signal: timeout,
       body: request ? JSON.stringify(request) : undefined,
       headers: {
         // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -55,7 +60,7 @@ export async function http(
       .then((res: any) => res.json());
   } else {
     return axios
-      .request({ url: url, method: method, data: request, headers: headers })
+      .request({ url: url, method: method, data: request, headers: headers, timeout })
       .then((res) => res.data);
   }
 }
