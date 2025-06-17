5c5465fcc0506229da5aef436d284c256d65666c
dynst <148708712+dynst@users.noreply.github.com>
2025-06-17 00:00:00 +0000

    use an address length limit compatible with ibc-go
    https://github.com/cosmos/ibc-go/blob/904047fc783f43ab87e3121c2c0c47ef7a6b075e/modules/apps/27-interchain-accounts/controller/types/msgs.go#L17

--- packages/faucet/src/addresses.ts
+++ packages/faucet/src/addresses.ts
@@ -1,12 +1,15 @@
 import { fromBech32 } from "@cosmjs/encoding";
 
 export function isValidAddress(input: string, requiredPrefix: string): boolean {
+  if (input.length > 2048) {
+    return false;
+  }
   try {
     const { prefix, data } = fromBech32(input);
     if (prefix !== requiredPrefix) {
       return false;
     }
-    return data.length >= 20 && data.length <= 32;
+    return data.length >= 20;
   } catch {
     return false;
   }
