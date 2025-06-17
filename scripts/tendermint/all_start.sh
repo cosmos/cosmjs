7596a46b6911664113cf4d45bac46a54981392db
Simon Warta <simon@warta.it>
2025-06-17 15:28:29 +0200

    Test Comet38Client against 0.38 and 1.x backends

--- scripts/tendermint/all_start.sh
+++ scripts/tendermint/all_start.sh
@@ -9,18 +9,21 @@ declare -a TM_IMAGES
 TM_IMAGES[34]="tendermint/tendermint:v0.34.19"
 TM_IMAGES[37]="cometbft/cometbft:v0.37.0-rc3"
 TM_IMAGES[38]="cometbft/cometbft:v0.38.0"
+TM_IMAGES[1]="cometbft/cometbft:v1.x"
 
 declare -a TM_ROOTS
 TM_ROOTS[34]="/tendermint"
 TM_ROOTS[37]="/cometbft"
 TM_ROOTS[38]="/cometbft"
+TM_ROOTS[1]="/cometbft"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for KEY in "${!TM_IMAGES[@]}"; do
+  PORT=$(printf "111%02g" "$KEY") # e.g. 11101 or 11138
   export TENDERMINT_IMAGE="${TM_IMAGES[$KEY]}"
   export TENDERMINT_ROOT="${TM_ROOTS[$KEY]}"
-  export TENDERMINT_PORT="111$KEY"
+  export TENDERMINT_PORT="$PORT"
   export TENDERMINT_NAME="tendermint-$KEY"
 
   echo "Starting $TENDERMINT_NAME ($TENDERMINT_IMAGE) on port $TENDERMINT_PORT ..."
