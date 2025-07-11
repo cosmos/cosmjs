9181df83f72b920003f7eb0e85de016a434fa0b1
Simon Warta <simon@warta.it>
2025-07-11 16:52:38 +0200

    Upgrade tar-fs

--- .pnp.cjs
+++ .pnp.cjs
@@ -2734,7 +2734,7 @@ const RAW_RUNTIME_STATE =
     ],\
     [\
       "tar-fs",\
-      "npm:2.1.1"\
+      "npm:2.1.3"\
     ],\
     [\
       "tar-stream",\
@@ -11096,7 +11096,7 @@ const RAW_RUNTIME_STATE =
           ["pump", "npm:3.0.0"],\
           ["rc", "npm:1.2.8"],\
           ["simple-get", "npm:4.0.1"],\
-          ["tar-fs", "npm:2.1.1"],\
+          ["tar-fs", "npm:2.1.3"],\
           ["tunnel-agent", "npm:0.6.0"]\
         ],\
         "linkType": "HARD"\
@@ -12197,13 +12197,13 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["tar-fs", [\
-      ["npm:2.1.1", {\
-        "packageLocation": "./.yarn/cache/tar-fs-npm-2.1.1-e374d3b7a2-871d26a934.zip/node_modules/tar-fs/",\
+      ["npm:2.1.3", {\
+        "packageLocation": "./.yarn/cache/tar-fs-npm-2.1.3-32d6d6fb31-472ee0c3c8.zip/node_modules/tar-fs/",\
         "packageDependencies": [\
           ["chownr", "npm:1.1.4"],\
           ["mkdirp-classic", "npm:0.5.3"],\
           ["pump", "npm:3.0.0"],\
-          ["tar-fs", "npm:2.1.1"],\
+          ["tar-fs", "npm:2.1.3"],\
           ["tar-stream", "npm:2.2.0"]\
         ],\
         "linkType": "HARD"\
