d708b6aab562a6242d07b821d8f570e9eb8a5413
fuder.eth <vtljgrinkiv@gmail.com>
2025-07-16 21:37:39 +0300

    Fix Typos in Comments and Documentation (#1677)
    * Update karma.conf.js
    
    * Update responses.ts
    
    * Update utf8.ts

--- packages/encoding/src/utf8.ts
+++ packages/encoding/src/utf8.ts
@@ -16,7 +16,7 @@ export function toUtf8(str: string): Uint8Array {
  * Takes UTF-8 data and decodes it to a string.
  *
  * In lossy mode, the [REPLACEMENT CHARACTER](https://en.wikipedia.org/wiki/Specials_(Unicode_block))
- * is used to substitude invalid encodings.
+ * is used to substitute invalid encodings.
  * By default lossy mode is off and invalid data will lead to exceptions.
  */
 export function fromUtf8(data: Uint8Array, lossy = false): string {
