889af67226c97650700c2c0398cb4b7e16d83c88
Micke <155267459+reallesee@users.noreply.github.com>
2025-07-21 09:45:28 +0200

    Update hex.ts

--- packages/encoding/src/hex.ts
+++ packages/encoding/src/hex.ts
@@ -1,30 +1,28 @@
-if (!Iterator.prototype.map) {
-  Iterator.prototype.map = function* (f) {
-    for (const value of this) {
-      yield f(value);
-    }
-  };
+function* map<T, U>(iterable: Iterable<T>, fn: (value: T) => U): IterableIterator<U> {
+  for (const value of iterable) {
+    yield fn(value);
+  }
 }
 
 export function toHex(data: Uint8Array): string {
-    return data.values()
-      .map((byte) => byte.toString(16).padStart(2, "0"))
-      .join("");
+  return Array.from(
+    map(data.values(), (byte) => byte.toString(16).padStart(2, "0"))
+  ).join("");
+}
+
+export function fromHex(hexstring: string): Uint8Array {
+  if (hexstring.length % 2 !== 0) {
+    throw new Error("hex string length must be a multiple of 2");
   }
-  
-  export function fromHex(hexstring: string): Uint8Array {
-    if (hexstring.length % 2 !== 0) {
-      throw new Error("hex string length must be a multiple of 2");
+
+  const out = new Uint8Array(hexstring.length / 2);
+  for (let i = 0; i < out.length; i++) {
+    const j = 2 * i;
+    const hexByteAsString = hexstring.slice(j, j + 2);
+    if (!hexByteAsString.match(/[0-9a-f]{2}/i)) {
+      throw new Error("hex string contains invalid characters");
     }
-  
-    const out = new Uint8Array(hexstring.length / 2);
-    for (let i = 0; i < out.length; i++) {
-      const j = 2 * i;
-      const hexByteAsString = hexstring.slice(j, j + 2);
-      if (!hexByteAsString.match(/[0-9a-f]{2}/i)) {
-        throw new Error("hex string contains invalid characters");
-      }
-      out[i] = parseInt(hexByteAsString, 16);
-    }
-    return out;
+    out[i] = parseInt(hexByteAsString, 16);
   }
+  return out;
+}
