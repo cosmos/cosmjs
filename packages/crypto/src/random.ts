e316d40e0f28585897a11e7bb8e71e305691b2ab
dynst <148708712+dynst@users.noreply.github.com>
2025-07-23 00:00:00 +0000

    simplify ancient class Random
    Node.js 20 implements the WebCrypto API.
    
    globalThis has existed since Node.js 12.

--- packages/crypto/src/random.ts
+++ packages/crypto/src/random.ts
@@ -1,27 +1,10 @@
-declare const window: any;
-declare const self: any;
-
 export class Random {
   /**
    * Returns `count` cryptographically secure random bytes
    */
   public static getBytes(count: number): Uint8Array {
-    try {
-      const globalObject = typeof window === "object" ? window : self;
-      const cryptoApi =
-        typeof globalObject.crypto !== "undefined" ? globalObject.crypto : globalObject.msCrypto;
-
-      const out = new Uint8Array(count);
-      cryptoApi.getRandomValues(out);
-      return out;
-    } catch {
-      try {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const crypto = require("crypto");
-        return new Uint8Array([...crypto.randomBytes(count)]);
-      } catch {
-        throw new Error("No secure random number generator found");
-      }
-    }
+    const out = new Uint8Array(count);
+    globalThis.crypto.getRandomValues(out);
+    return out;
   }
 }
