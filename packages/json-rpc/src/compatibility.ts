a2a16b55b40bf0a9aec4628b6e223293b734e1e0
dynst <148708712+dynst@users.noreply.github.com>
2025-08-12 00:00:00 +0000

    remove redundancy from JsonCompatibleDictionary

--- packages/json-rpc/src/compatibility.ts
+++ packages/json-rpc/src/compatibility.ts
@@ -19,7 +19,7 @@ export interface JsonCompatibleArray extends ReadonlyArray<JsonCompatibleValue> 
  * A string to json value dictionary.
  */
 export interface JsonCompatibleDictionary {
-  readonly [key: string]: JsonCompatibleValue | readonly JsonCompatibleValue[];
+  readonly [key: string]: JsonCompatibleValue;
 }
 
 export function isJsonCompatibleValue(value: unknown): value is JsonCompatibleValue {
