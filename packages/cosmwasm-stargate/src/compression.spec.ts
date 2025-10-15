fbddcb3558f4ec049bb5022ed3e8c02a7f67a770
Simon Warta <simon@warta.it>
2025-10-15 14:21:39 +0200

    Improve gzip testing code

--- packages/cosmwasm-stargate/src/compression.spec.ts
+++ packages/cosmwasm-stargate/src/compression.spec.ts
@@ -1,4 +1,5 @@
 import { fromHex } from "@cosmjs/encoding";
+import { Uint32 } from "@cosmjs/math";
 
 import { gzip } from "./compression";
 
@@ -12,7 +13,7 @@ async function uncompress(compressed: Uint8Array): Promise<Uint8Array> {
 describe("compression", () => {
   describe("gzip", () => {
     it("works", async () => {
-      const data = [
+      const tests = [
         Uint8Array.from([]),
         Uint8Array.from([0xaa]),
         Uint8Array.from([0xaa, 0xbb]),
@@ -26,13 +27,17 @@ describe("compression", () => {
         new Uint8Array(135454).fill(45),
         new Uint8Array(1484384).fill(46),
       ];
-      for (const [index, d] of data.entries()) {
-        const compressed = await gzip(d);
-        // "A gzip file contains: a 10-byte header. This header contains its magic number (“1F8B08”),
-        // a version number and a time stamp, [...]" https://bits.ashleyblewer.com/blog/2024/01/12/researching-file-formats-20-gzip/
-        expect(compressed.length).withContext(`Data with index ${index}`).toBeGreaterThanOrEqual(10);
+      for (const [index, original] of tests.entries()) {
+        const compressed = await gzip(original);
+        // "A gzip file contains:
+        //  - a 10-byte header. This header contains its magic number (“1F8B08”), a version number and a time stamp,
+        //  - [...]
+        //  - and an 8-byte footer, containing a CRC-32 checksum and the length of the original uncompressed data."
+        // https://bits.ashleyblewer.com/blog/2024/01/12/researching-file-formats-20-gzip/
+        expect(compressed.length).withContext(`Data with index ${index}`).toBeGreaterThanOrEqual(18);
         expect(compressed.slice(0, 3)).toEqual(fromHex("1F8B08"));
-        expect(await uncompress(compressed)).toEqual(d);
+        expect(compressed.subarray(-4)).toEqual(new Uint32(original.length).toBytesLittleEndian());
+        expect(await uncompress(compressed)).toEqual(original);
       }
     });
   });
