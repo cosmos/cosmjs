148c45df86f237ad6ee03c9e23d3aafa3c21c89e
Simon Warta <simon@warta.it>
2025-10-29 11:15:11 +0100

    Fix typo startusResponse -> statusResponse
    Closes #1887

--- packages/tendermint-rpc/src/rpcclients/websocketclient.spec.ts
+++ packages/tendermint-rpc/src/rpcclients/websocketclient.spec.ts
@@ -135,8 +135,8 @@ import { WebsocketClient } from "./websocketclient";
 
     client
       .execute(createJsonRpcRequest("status"))
-      .then((startusResponse) => {
-        expect(startusResponse).toBeTruthy();
+      .then((statusResponse) => {
+        expect(statusResponse).toBeTruthy();
       })
       .catch(done.fail);
 
